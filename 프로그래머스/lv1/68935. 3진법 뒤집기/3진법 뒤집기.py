f554d0257a28d4251bbc7869cdd9bbf565f73ce0
dumbbellpullover <79296295+dumbbellpullover@users.noreply.github.com>
2023-01-03 01:24:51 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,11 +1,8 @@
 def solution(n):
     answer = 0
     a = ''
-    while n // 3 >= 1:
+    while n:
         a += str(n % 3)
         n //= 3
-    a += str(n % 3)
     
-    answer = sum([int(a[-i-1])*3**i for i in range(len(a))])
-    
-    return answer
+    return int(a, 3)
