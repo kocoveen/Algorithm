2a59fad6ae5ba7b45119307f1948f31f4ea22de5
dumbbellpullover <79296295+dumbbellpullover@users.noreply.github.com>
2023-01-03 21:23:23 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,12 +1,20 @@
 def solution(N, stages):
-    a = {}
-    answer = [len(stages)]
-    for i in range(1, N+1):
-        answer.append(answer[-1] - stages.count(i))
+    answer = []
+    fail = []
+    info = [0] * (N + 2)
     
+    for stage in stages:
+        info[stage] += 1
+        
     for i in range(N):
-        if answer[i] != 0:
-            a[i+1] = stages.count(i+1) / answer[i]
+        be = sum(info[(i + 1):])
+        yet = info[i + 1]
+        if be == 0:
+            fail.append((i + 1, 0))
         else:
-            a[i+1] = 0
-    return [i[0] for i in sorted(a.items(), key=lambda item: item[1], reverse=True)]
+            fail.append((i + 1, yet / be))
+
+    for item in sorted(fail, key=lambda x: x[1], reverse=True):
+        answer.append(int(item[0]))
+
+    return answer
