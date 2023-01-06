18874ad5f977a5e285ebe27c229734b5dbfddb51
dumbbellpullover <79296295+dumbbellpullover@users.noreply.github.com>
2023-01-06 16:47:20 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,7 +1,3 @@
 def solution(numbers):
-    answer = 0
     numbers=sorted(numbers)
-    if numbers[0]*numbers[1] > numbers[-2]*numbers[-1]:
-        return numbers[0]*numbers[1]
-    else:
-        return numbers[-2]*numbers[-1]
+    return max(numbers[0]*numbers[1], numbers[-2]*numbers[-1])
