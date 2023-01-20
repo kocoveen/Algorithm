3a2c05c8fe376e544de41d6aa6aad07880c50f44
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-01-21 01:57:10 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.11 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for ( int i = 1; i <= n; i++) {
+            answer += 1;
+            while (String.valueOf(answer).contains("3") || answer % 3 == 0) {
+                answer += 1;
+            }
+        }
+        
+            
+        return answer;
+    }
+}
