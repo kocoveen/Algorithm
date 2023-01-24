fc2ab6827130ae00758fdf3fedd90ad3f53c4c53
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-01-25 01:44:14 +0900

    [level 0] Title: 옹알이 (1), Time: 0.66 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,10 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        
-        String[] pronounce = {"aya", "ye", "woo", "ma"};
         
         for (String bab : babbling) {
             String[] a = bab.split("(aya|ye|woo|ma)");
