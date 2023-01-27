df6bdc91d36d6bb2cd99ebd1cb0b9ae53e3b0785
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-01-27 15:20:11 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
@@ -1,36 +1,5 @@
 class Solution {
     public String solution(String bin1, String bin2) {
-        String answer = "";
-        int dec1 = 0;
-        int dec2 = 0;
-        
-        dec1 = binToDec(bin1);
-        dec2 = binToDec(bin2);      
-        answer = decToBin(dec1+dec2);
-        
-        return answer;
-    }
-    
-    public int binToDec(String bin) {
-        int dec = 0;
-        int mult = 1;
-        for (int i = bin.length()-1; i >= 0; i--) {
-            dec += (bin.charAt(i) == '1' ? mult : 0);
-            mult *= 2;
-        }
-        return dec;
-    }
-    
-    public String decToBin(int dec) {
-        String bin = "";
-        while(dec > 0) {
-            bin = (String.valueOf(dec % 2)) + bin;
-            dec /= 2;
-        }
-        
-        if (bin.equals("")) {
-            bin += "0";
-        }
-        return bin;
+        return Integer.toString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2), 2);
     }
 }
