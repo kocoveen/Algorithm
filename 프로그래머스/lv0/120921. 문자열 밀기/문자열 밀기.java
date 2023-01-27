bbd8e6a9a9f5eb58373690b24b1532ee7bde227d
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-01-27 16:26:07 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,15 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = -1;
-        String A2 = "";
-        A2 = A + A;
-        
-        for (int i = A2.length(); i > A.length(); i--) {
-            if (A2.substring(i-A.length(), i).contains(B)) {
-                return A2.length()-i;
-            }
-        }
-        
-        return answer;
+        String T = B.repeat(2);
+        return T.indexOf(A);
     }
 }
