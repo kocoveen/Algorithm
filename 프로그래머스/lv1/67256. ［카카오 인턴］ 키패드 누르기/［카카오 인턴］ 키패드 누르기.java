449604f386be1f8674b1b065c40560a007bbba36
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-01-30 15:39:24 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 3.04 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,68 +1,50 @@
-import java.lang.Math;
-
 class Solution {
+    //        0부터 9까지 좌표 {y,x}
+    int[][] numpadPos = {
+            {3,1}, //0
+            {0,0}, //1
+            {0,1}, //2
+            {0,2}, //3
+            {1,0}, //4
+            {1,1}, //5
+            {1,2}, //6
+            {2,0}, //7
+            {2,1}, //8
+            {2,2}  //9
+    };
+    //초기 위치
+    int[] leftPos = {3,0};
+    int[] rightPos = {3,2};
+    String hand;
     public String solution(int[] numbers, String hand) {
-        String answer = "";
-        int[] L = {0, 3};
-        int[] R = {2, 3};
-        int[][] coord = new int[12][2];
-        
-        for (int i=0; i<12; i++) {
-            if (i == 0 || i > 9) {
-                coord[i][1] = 3;
-                switch (i) {
-                    case 0:
-                        coord[i][0] = 1;
-                        break;
-                    case 10: 
-                        coord[i][0] = 0;
-                        break;
-                    case 11: 
-                        coord[i][0] = 2;
-                        break;
-                }
-            } else if (i > 0 && i < 10) {
-                coord[i][0] = (i+2) % 3;
-                coord[i][1] = (i-1) / 3;
-            }
-        }
-        
-        for (int num : numbers) {
-            if(num % 3 == 1) {
-                L[0] = coord[num][0];
-                L[1] = coord[num][1];
-                answer += "L";
-            } else if (num % 3 == 0 && num != 0) {
-                R[0] = coord[num][0];
-                R[1] = coord[num][1];
-                answer += "R";
-            } else {
-                if (Math.abs(L[0]-coord[num][0]) + Math.abs(L[1]-coord[num][1]) > Math.abs(R[0]-coord[num][0]) + Math.abs(R[1]-coord[num][1])) {
-                    R[0] = coord[num][0];
-                    R[1] = coord[num][1];
-                    answer += "R";
-                } else if (Math.abs(L[0]-coord[num][0]) + Math.abs(L[1]-coord[num][1]) < Math.abs(R[0]-coord[num][0]) + Math.abs(R[1]-coord[num][1])) {
-                    L[0] = coord[num][0];
-                    L[1] = coord[num][1];
-                    answer += "L";
-                } else {
-                    switch (hand) {
-                        case "left":
-                            L[0] = coord[num][0];
-                            L[1] = coord[num][1];
-                            answer += "L";
-                            break;
-                        case "right":
-                            R[0] = coord[num][0];
-                            R[1] = coord[num][1];
-                            answer += "R";
-                            break;
-                    }
-                }
+        this.hand = (hand.equals("right")) ? "R" : "L";
 
-            }
+        String answer = "";
+        for (int num : numbers) {
+            String Umji = pushNumber(num);
+            answer += Umji;
+
+            if(Umji.equals("L")) {leftPos = numpadPos[num]; continue;}
+            if(Umji.equals("R")) {rightPos = numpadPos[num]; continue;}
         }
-        
         return answer;
+    }
+
+    //num버튼을 누를 때 어디 손을 사용하는가
+    private String pushNumber(int num) {
+        if(num==1 || num==4 || num==7) return "L";
+        if(num==3 || num==6 || num==9) return "R";
+
+        // 2,5,8,0 일때 어디 손가락이 가까운가
+        if(getDist(leftPos, num) > getDist(rightPos, num)) return "R";
+        if(getDist(leftPos, num) < getDist(rightPos, num)) return "L";
+
+        //같으면 손잡이
+        return this.hand;
+    }
+
+    //해당 위치와 번호 위치의 거리
+    private int getDist(int[] pos, int num) {
+        return Math.abs(pos[0]-numpadPos[num][0]) + Math.abs(pos[1]-numpadPos[num][1]);
     }
 }
