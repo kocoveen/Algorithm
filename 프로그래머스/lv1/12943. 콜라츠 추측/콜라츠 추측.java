90878ae7a6a5d15a4250d932558ee41f1e8a68fe
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-02-09 22:02:41 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,17 +1,15 @@
 class Solution {
     public int solution(long num) {
         int answer = 0;
-        while (answer <= 500) {
-            if (num == 1) break;
+        while (answer <= 501) {
+            if (num == 1) return answer;
+            if (answer > 500) return -1;
             
             answer += 1;
             
-            if (num % 2 == 0) num /= 2;
-            else if (num % 2 != 0) num = num * 3 + 1;
-
+            num = (num % 2 == 0) ? (num / 2) : (num * 3 + 1);
         }
         
-        if (answer > 500) answer = -1;
         return answer;
     }
 }
