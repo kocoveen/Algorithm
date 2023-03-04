908897fa1498f3e539a2bbff7ca0fb1b39f83e69
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-03-04 18:02:32 +0900

    [unrated] Title: 콜라 문제, Time: 0.01 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -1,22 +1,5 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        int answer = 0;
-        int cola = n;
-        int quotient = 0;
-        int remainder = 0;
-        
-        while(true) {
-            quotient = (int) cola / a;
-            remainder = cola % a;
-            
-            answer += quotient * b;
-            cola = quotient * b + remainder;
-            
-            if(quotient == 0) {
-                break;
-            }
-        }
-        
-        return answer;
+        return (n - b) / (a - b) * b;
     }
 }
