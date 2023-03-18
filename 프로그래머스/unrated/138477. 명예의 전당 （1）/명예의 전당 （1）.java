04df27be1b19e43a2a5bd2afbff54f5173859a7f
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-03-19 00:34:19 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.29 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,24 +3,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        List<Integer> hof = new ArrayList<>();
         
-        for (int i = 0; i < score.length; i++) {
-            hof.add(Integer.valueOf(score[i]));
-            
-            if(i > 0) {
-                Collections.sort(hof);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i = 0; i < score.length; i++) {
+            pq.add(score[i]);
+            if (pq.size() > k) {
+                pq.poll();
             }
-            
-            if(hof.size() > k) {
-                hof.remove(0);
-            }
-                        
-            answer[i] = hof.get(0).intValue();
+            answer[i] = pq.peek();
         }
-        
-        
-        
         return answer;
     }
 }
