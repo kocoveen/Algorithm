393fbf0cef8e40263062c0a3e92c3a5743754bfc
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-03-25 20:49:16 +0900

    [unrated] Title: 덧칠하기, Time: 0.81 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int last = 0;
+        
+        for (int s : section) {
+            if (s > last) {
+                answer++;
+                last = s + m - 1;
+            }
+        }
+        
+        return answer;
+    }
+}
