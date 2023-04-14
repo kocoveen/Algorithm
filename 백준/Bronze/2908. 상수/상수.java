ec3ca536ac49484492fefc8b6def43e10fddba44
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-04-14 23:38:35 +0900

    [Bronze II] Title: 상수, Time: 136 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -5,21 +5,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer sb;
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         String a = st.nextToken();
         String b = st.nextToken();
 
-        sb = new StringBuffer(a);
-        int i = Integer.parseInt(String.valueOf(sb.reverse()));
-        sb = new StringBuffer(b);
-        int j = Integer.parseInt(String.valueOf(sb.reverse()));
+        char aC[] = a.toCharArray();
+        for (int i = 0; i < aC.length/2; i++) {
+            char temp = aC[i];
+            aC[i] = aC[aC.length-1+i];
+            aC[aC.length-1+i] = temp;
+        }
+        int ai = Integer.parseInt(new String(aC));
 
-        if(i > j)
-            bw.write(i +"");
+        char bC[] = b.toCharArray();
+        for (int i = 0; i < bC.length/2; i++) {
+            char temp = bC[i];
+            bC[i] = bC[bC.length-1+i];
+            bC[bC.length-1+i] = temp;
+        }
+        int bi = Integer.parseInt(new String(bC));
+
+        if (ai > bi)
+            bw.write(ai + "");
         else
-            bw.write(j +"");
+            bw.write(bi + "");
 
         br.close();
         bw.flush();
