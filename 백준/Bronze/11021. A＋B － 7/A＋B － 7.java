dee62d3d56afe55c2141a8d33274a2c517ffbf4b
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-04-14 14:08:36 +0900

    [Bronze V] Title: A+B - 7, Time: 292 ms, Memory: 18192 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.printf("Case #%d: %d\n", i+1, a+b);
+        }
+    }
+}
