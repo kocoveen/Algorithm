3206cd5131f368bb7d1b3996e539cbc346448a1e
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-04-14 14:13:04 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 216 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -6,10 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j < i+1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            System.out.println("*".repeat(i));
         }
     }
 }
