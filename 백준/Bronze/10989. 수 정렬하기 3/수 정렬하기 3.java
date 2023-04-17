895a2da43ad4b2db450fb2b383f76538ba5eb498
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-04-18 01:15:32 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2004 ms, Memory: 379020 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -8,17 +7,38 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int[] A = new int[N];
 
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(br.readLine());
+        } 
 
-        Arrays.sort(A);
+        countingSort(A);
 
-        for (int i : A) {
+        for (int i : result) {
             bw.write(i + "\n");
         }
 
         br.close();
         bw.flush();
         bw.close();
+    }
+
+    private static int[] counting;
+    private static int[] result;
+
+    private static void countingSort(int[] A) {
+        counting = new int[10001];
+        result = new int[A.length];
+
+        for (int j : A)
+            counting[j]++;
+
+        for (int i = 0; i < counting.length - 1; i++)
+            counting[i+1] += counting[i];
+
+        for (int i = A.length - 1; i >= 0; i--) {
+            int value = A[i];
+            counting[value]--;
+            result[counting[value]] = value;
+        }
     }
 }
