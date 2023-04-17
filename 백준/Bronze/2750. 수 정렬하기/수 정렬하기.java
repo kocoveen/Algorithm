b52f8f7cbe543c8ee78172aede44fcb7ead03d9d
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-04-17 20:20:07 +0900

    [Bronze II] Title: 수 정렬하기, Time: 168 ms, Memory: 16120 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+        bubbleSort(arr);
+        for (int i : arr) {
+            bw.write(i + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                }
+            }
+        }
+    }
+}
