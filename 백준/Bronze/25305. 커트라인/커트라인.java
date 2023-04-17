6bdfad3a84166f2ec78ebf81596ead8a2da30d40
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-04-17 20:42:33 +0900

    [Bronze II] Title: 커트라인, Time: 152 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        bubbleSort(arr);
+
+        bw.write(arr[k-1] + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[j - 1] < arr[j]) {
+                    int t = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = t;
+                }
+            }
+        }
+    }
+}
+
