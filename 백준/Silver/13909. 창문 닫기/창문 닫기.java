bc3be89870c92e237c7944dc043980ffb6ecf496
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-04-19 23:39:01 +0900

    [Silver V] Title: 창문 닫기, Time: 140 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.java
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -4,12 +4,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(br.readLine());
-        int count = 0;
-        for (int i = 1, n = 1; i <= N; i += 2 * n + 1, n++)
-            count++;
-        bw.write(count + "");
+        bw.write((int) Math.floor(Math.sqrt(N)) + "");
         bw.flush();
         br.close();
         bw.close();
