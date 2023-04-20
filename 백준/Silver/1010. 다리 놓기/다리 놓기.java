aedd92c89e1f97f790a311cbd20c3e4aa68d2669
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-04-20 17:02:13 +0900

    [Silver V] Title: 다리 놓기, Time: 128 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -2,19 +2,19 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[][] memo = new int[30][30];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int left = Integer.parseInt(st.nextToken());
-            int right = Integer.parseInt(st.nextToken());
-            long result = bridge(left, right);
-            sb.append(result).append("\n");
+            int r = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            sb.append(combination(n, r)).append('\n');
         }
 
         bw.write( sb + "");
@@ -23,14 +23,11 @@ public class Main {
         br.close();
         bw.close();
     }
-
-    private static long bridge(long left, long right) {
-        long result = 1;
-        long temp = Math.max(right - left, left);
-        for (long i = right; i > temp; i--)
-            result *= i;
-        for (int i = 2; i <= right - temp; i++)
-            result /= i;
-        return result;
+    private static int combination(int n, int r) {
+        if(memo[n][r] > 0)
+            return memo[n][r];
+        if(n == r || r == 0)
+            return memo[n][r] = 1;
+        return memo[n][r] = combination(n - 1, r - 1) + combination(n - 1, r);
     }
 }
