70e78badccf65b302482325bd7a6445736217993
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-04-20 18:20:02 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 296 ms, Memory: 25536 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -1,30 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        Map<String, String> nick = new HashMap<>();
+        Set<String> nick = new HashSet<>();
 
         int count = 0;
         for (int i = 0; i < N; i++) {
             String log = br.readLine();
             if (log.equals("ENTER")) {
-                count += nick.size();
                 nick.clear();
                 continue;
             }
-            if (!nick.containsKey(log))
-                nick.put(log, log);
+            if(!nick.contains(log))
+                count++;
+            nick.add(log);
         }
-        count += nick.size();
         System.out.println(count);
     }
 }
