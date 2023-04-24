13bd8f11893c918b79ffc93e8992419f502a497e
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-04-24 16:42:20 +0900

    [Silver III] Title: N과 M (3), Time: 484 ms, Memory: 68268 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int[] Num;
+    static int N;
+    static int M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        Num = new int[M];
+
+        findNum(0);
+        System.out.print(sb);
+
+    }
+
+    public static void findNum(int depth) {
+        if (depth == M) {
+            for (int i : Num)
+                sb.append(i).append(' ');
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            Num[depth] = i;
+            findNum(depth + 1);
+        }
+    }
+}
