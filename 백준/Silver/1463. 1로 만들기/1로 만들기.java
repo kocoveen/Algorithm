cebfa251f0c419a7e56fd98203cd9c06635d0b76
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-04-27 20:52:17 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 19776 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -13,24 +13,16 @@ public class Main {
         dp = new Integer[N+1];
         dp[0] = dp[1] = 0;
 
-        bw.write(findMax(N) + "\n");
+        bw.write(findMax(N, 0) + "\n");
 
         br.close();
         bw.flush();
         bw.close();
     }
 
-    public static int findMax(int N) {
-        if (dp[N] == null) {
-            if (N % 6 == 0)
-                dp[N] = Math.min(findMax(N / 3), Math.min(findMax(N / 2), findMax(N - 1))) + 1;
-            else if (N % 3 == 0)
-                dp[N] = Math.min(findMax(N / 3), findMax(N - 1)) + 1;
-            else if (N % 2 == 0)
-                dp[N] = Math.min(findMax(N / 2), findMax(N - 1)) + 1;
-            else
-                dp[N] = findMax(N - 1) + 1;
-        }
-        return dp[N];
+    public static int findMax(int N, int count) {
+        if (N < 2)
+            return count;
+        return Math.min(findMax(N / 2, count + 1 + (N % 2)), findMax(N / 3, count + 1 + (N % 3)));
     }
 }
