9977c53c54e273669fcfdc4b98dffb8c8eeca435
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-05-08 23:37:53 +0900

    [Silver II] Title: 에디터, Time: 444 ms, Memory: 49824 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -4,20 +4,20 @@ import java.util.*;
 public class Main {
     static StringBuilder sb = new StringBuilder();
 //    static StringTokenizer st;
-    static int cursor;
+    public final static int CAPACITY = 600001;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        WildList wl = new WildList(CAPACITY);
         String s = br.readLine();
-        Stack<Character> L = new Stack<>();
-        Stack<Character> R = new Stack<>();
+        int cursor = 0;
 
-        for (int i = 0; i < s.length(); i++)
-            L.push(s.charAt(i));
-
-        cursor = L.size();
+        for (int i = 0; i < s.length(); i++) {
+            wl.insert(i, s.charAt(i));
+            cursor++;
+        }
 
         int N = Integer.parseInt(br.readLine());
 
@@ -25,27 +25,70 @@ public class Main {
             String cmd = br.readLine();
             switch (cmd.charAt(0)) {
                 case 'L' -> {
-                    if (!L.isEmpty()) R.add(L.pop());
+                    if (wl.prev[cursor] != -1) cursor = wl.prev[cursor];
                 }
                 case 'D' -> {
-                    if (!R.isEmpty()) L.add(R.pop());
+                    if (wl.next[cursor] != -1) cursor = wl.next[cursor];
                 }
                 case 'B' -> {
-                    if (!L.isEmpty()) L.pop();
+                    if (wl.prev[cursor] != -1) {
+                        wl.erase(cursor);
+                        cursor = wl.prev[cursor];
+                    }
                 }
-                case 'P' -> L.add(cmd.charAt(2));
+                case 'P' -> {
+                    wl.insert(cursor, cmd.charAt(2));
+                    cursor = wl.next[cursor];
+                }
             }
         }
 
-        for (Character c : L)
-            sb.append(c);
-        while (!R.isEmpty())
-            sb.append(R.pop());
-
+        wl.traverse();
         bw.write(sb + "");
 
         bw.flush();
         br.close();
         bw.close();
+    }
+
+    public static class WildList {
+        char[] data;
+        int[] prev;
+        int[] next;
+        int unused;
+
+        public WildList(int capacity) {
+            data = new char[capacity];
+            prev = new int[capacity];
+            next = new int[capacity];
+
+            Arrays.fill(prev, -1);
+            Arrays.fill(next, -1);
+
+            unused = 1;
+        }
+
+        public void insert(int addr, char val) {
+            data[unused] = val;
+            prev[unused] = addr;
+            next[unused] = next[addr];
+            if (next[addr] != -1) prev[next[addr]] = unused;
+            next[addr] = unused;
+            unused++;
+        }
+
+        public char erase(int addr) {
+            next[prev[addr]] = next[addr];
+            if (next[addr] != -1) prev[next[addr]] = prev[addr];
+            return data[addr];
+        }
+
+        public void traverse() {
+            int cursor = next[0];
+            while (cursor != -1) {
+                sb.append(data[cursor]);
+                cursor = next[cursor];
+            }
+        }
     }
 }
