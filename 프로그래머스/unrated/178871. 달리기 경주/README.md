0753847f76f2096869ddbe4b6efdf98532491c8d
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-05-11 01:20:28 +0900

    [unrated] Title: 달리기 경주, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/README.md
+++ 프로그래머스/unrated/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [unrated] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871) 
 
 ### 성능 요약
 
-메모리: 71.8 MB, 시간: 0.05 ms
+메모리: 80.1 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -2,23 +2,21 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = {};
         
-        Map<String, Integer> rank = new HashMap<>();
+        Map<String, Integer> rankMap = new HashMap<>();
         
         for (int i = 0; i < players.length; i++)
-            rank.put(players[i], i);
+            rankMap.put(players[i], i);
         
-        for (int i = 0; i < callings.length; i++) {
+        for (String calling : callings) {
+            int rank = rankMap.get(calling);
             
-            int score = rank.get(callings[i]);
+            String temp = players[rank];
+            players[rank] = players[rank - 1];
+            players[rank - 1] = temp;
             
-            String temp = players[score];
-            players[score] = players[score - 1];
-            players[score - 1] = temp;
-            
-            rank.replace(callings[i], score - 1);
-            rank.replace(players[score], rank.get(players[score]) + 1);
+            rankMap.put(players[rank - 1], rank - 1);
+            rankMap.put(players[rank], rank);
         }
         
         return players;
