588070e82ba41ab859529a70c21fbc3df985ee31
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-05-12 15:09:10 +0900

    [Silver I] Title: 숨바꼭질, Time: 200 ms, Memory: 61396 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,12 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
@@ -19,33 +17,16 @@ public class Main {
         location[N] = 0;
         L.add(N);
 
-        if (L.peek() == K) {
-            System.out.println(0);
-            System.exit(0);
-        }
-
-        while (!L.isEmpty()) {
+        while (location[K] == null) {
             int cur = L.remove();
-            for (int i = 1; i <= 3; i++) {
-                int nx = operate(cur, i);
+            for (int nx : new int[] {cur - 1, cur + 1, cur * 2}) {
                 if (nx < 0 || nx > 100000) continue;
                 if (location[nx] != null) continue;
-
                 location[nx] = location[cur] + 1;
-                if (nx == K) {
-                    System.out.println(location[nx]);
-                    System.exit(0);
-                }
                 L.add(nx);
-
             }
         }
-    }
 
-    private static int operate(int cur, int i) {
-        if (i == 1) return cur - 1;
-        if (i == 2) return cur + 1;
-        return cur * 2;
+        System.out.println(location[K]);
     }
-
 }
