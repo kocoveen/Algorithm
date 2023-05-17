4c94a7c4c8a022738388c94e66234a4d1d19aa51
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-05-17 21:10:54 +0900

    [Silver I] Title: 곱셈, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,33 +1,21 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-
-        System.out.println(power(A, B, C) % C);
+        String[] l = br.readLine().split(" ");
+        int A = Integer.parseInt(l[0]);
+        int B = Integer.parseInt(l[1]);
+        int C = Integer.parseInt(l[2]);
+        System.out.println(power(A, B, C));
     }
 
-    public static long power(int A, int B, int C) {
-        if (B == 1)
-            return A;
-
-        if (B % 2 == 0) {
-            long tmp = power(A, B / 2, C);
-            return (tmp * tmp) % C;
-        } else {
-            long tmp = (power(A, (B - 1) / 2, C));
-            return (tmp % C * tmp % C * A % C) % C;
-        }
+    private static long power(int A, int B, int C) {
+        if (B == 1) return A % C;
+        long tmp = power(A, B / 2, C);
+        tmp = tmp * tmp % C;
+        if (B % 2 == 0) return tmp;
+        return tmp * A % C;
     }
-
 }
