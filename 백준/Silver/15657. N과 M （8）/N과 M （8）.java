8da931afe4b0ab8be1b4feee6b1b674a65b12e4e
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-05-20 00:52:15 +0900

    [Silver III] Title: N과 M (8), Time: 180 ms, Memory: 18512 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+
+    static int[] arr;
+    static int[] tmp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] l1 = br.readLine().split(" ");
+        N = Integer.parseInt(l1[0]);
+        M = Integer.parseInt(l1[1]);
+
+        arr = new int[N];
+        tmp = new int[M];
+
+        String[] l2 = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(l2[i]);
+        Arrays.sort(arr);
+
+        func(0, 0);
+        System.out.print(sb);
+    }
+
+    private static void func(int depth, int s) {
+        if (depth == M) {
+            for (int i : tmp)
+                sb.append(i).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = s; i < N; i++) {
+            tmp[depth] = arr[i];
+            func(depth + 1, i);
+        }
+    }
+}
