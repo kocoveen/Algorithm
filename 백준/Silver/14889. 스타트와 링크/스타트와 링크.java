cdbb413c52e499aae1bd44273a83f1a6586fc611
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-05-28 20:25:00 +0900

    [Silver II] Title: 스타트와 링크, Time: 372 ms, Memory: 18432 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,65 +1,49 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
-    static int[][] A;
-    static int[] isVisited;
-    static int N;
-    static int min = Integer.MAX_VALUE;
+    static String[] l;
+
+    static int N, minDiff = Integer.MAX_VALUE;
+
+    static int[][] board;
+    static boolean[] vis;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
 
-        A = new int[N][N];
-        isVisited = new int[N];
+        board = new int[N][N];
+        vis = new boolean[N];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++)
-                A[i][j] = Integer.parseInt(st.nextToken());
+            l = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) board[i][j] = Integer.parseInt(l[j]);
         }
 
-        dfs(0, 0);
-        System.out.print(min);
+        func(0, 0);
+        System.out.println(minDiff);
     }
 
-    public static void dfs(int startDepth, int startPlayer) {
-        if (startDepth == N/2) {
-            int diff = stat();
-            if (min > diff)
-                min = diff;
-            if (min == 0) {
-                System.out.println(min);
-                System.exit(0);
-            }
+    private static void func(int depth, int startPlayer) {
+        if (depth == N / 2) {
+            minDiff = Math.min(minDiff, stat());
             return;
         }
+
         for (int i = startPlayer; i < N; i++) {
-            if (isVisited[i] == 1) continue;
-            isVisited[i] = 1;
-            dfs(startDepth + 1, i + 1);
-            isVisited[i] = 0;
+            vis[i] = true;
+            func(depth + 1, i + 1);
+            vis[i] = false;
         }
     }
 
-    public static int stat() {
-        int team_start = 0;
-        int team_link = 0;
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (isVisited[i] == 1 && isVisited[j] == 1) {
-                    team_start += A[i][j];
-                    team_start += A[j][i];
-                }
-                else if (isVisited[i] == 0 && isVisited[j] == 0) {
-                    team_link += A[i][j];
-                    team_link += A[j][i];
-                }
+    private static int stat() {
+        int star = 0, link = 0;
+        for (int i = 0; i < N - 1; i++)
+            for (int j = i; j < N; j++) {
+                if (vis[i] && vis[j]) star += board[i][j] + board[j][i];
+                else if (!vis[i] && !vis[j]) link += board[i][j] + board[j][i];
             }
-        }
-        return Math.abs(team_link - team_start);
+        return Math.abs(star - link);
     }
 }
