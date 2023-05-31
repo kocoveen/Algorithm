185cae618c5ba9e3f714b963e31853d348651fc4
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-05-31 18:06:35 +0900

    [Silver III] Title: 파도반 수열, Time: 132 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,39 +1,29 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
-    static long[] l;
+    static String[] l;
+
+    static int T;
+
+    static Long[] dp = new Long[105];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int T = Integer.parseInt(br.readLine());
+        T = Integer.parseInt(br.readLine());
 
-        l = new long[101];
-        l[0] = 0;
-        l[1] = 1;
-        l[2] = 1;
+        dp[0] = 0L;
+        dp[1] = 1L;
+        dp[2] = 1L;
+        dp[3] = 1L;
 
-        for (int i = 3; i < 101; i++)
-            l[i] = -1;
-
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
-            bw.write(fib(N) + "\n");
-        }
-
-
-        br.close();
-        bw.flush();
-        bw.close();
+        while (T-- > 0) System.out.println(func(Integer.parseInt(br.readLine())));
     }
 
-    public static long fib(int N) {
-        if (l[N] == -1)
-            l[N] = fib(N-2) + fib(N-3);
-        return l[N];
+    private static long func(int N) {
+        if (dp[N] == null) {
+            dp[N] = func(N - 2) + func(N - 3);
+        }
+        return dp[N];
     }
 }
