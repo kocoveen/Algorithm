a45d02a54458cf120215c34335e9bef736cacaf7
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-06-01 14:53:56 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,41 +1,31 @@
 import java.io.*;
 
 public class Main {
-    static Long[][] dp;
     static int N;
+    static long C = 1000000000L;
     static long sum;
+
+    static Long[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
 
-        dp = new Long[N+1][10];
+        dp = new Long[N + 1][10];
 
-        for (int i = 0; i <= 9; i++)
-            dp[1][i] = 1L;
+        for (int i = 0; i < 10; i++) dp[1][i] = 1L;
 
-        for (int i = 1; i <= 9; i++)
-            sum += findMax(N, i);
-
-        bw.write(sum % 1000000000L + "\n");
-
-        br.close();
-        bw.flush();
-        bw.close();
+        for (int i = 1; i < 10; i++) sum += func(N, i);
+        System.out.println(sum % C);
     }
 
-    public static long findMax(int N, int index) {
-        if (dp[N][index] == null) {
-            if (index == 0)
-                return dp[N][index] = findMax(N - 1, index + 1);
-
-            if (index == 9)
-                return dp[N][index] = findMax(N - 1, index - 1);
-
-            dp[N][index] = findMax(N - 1, index - 1) + findMax(N - 1, index + 1);
+    private static long func(int n, int i) {
+        if (dp[n][i] == null) {
+            if (i == 0) return dp[n][i] = func(n - 1, i + 1);
+            if (i == 9) return dp[n][i] = func(n - 1, i - 1);
+            dp[n][i] = func(n - 1, i - 1) + func(n - 1, i + 1);
         }
-        return dp[N][index] % 1000000000L;
+        return dp[n][i] % C;
     }
 }
