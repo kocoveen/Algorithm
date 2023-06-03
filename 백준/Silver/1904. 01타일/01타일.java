1fda2362a6c91b9f3cf06d5977cafc0d365f0193
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-06-03 21:13:11 +0900

    [Silver III] Title: 01타일, Time: 152 ms, Memory: 18120 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,34 +1,22 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
-    static int[] tile;
+    static String[] l;
+
+    static int N, C = 15746;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
+        l = br.readLine().split(" ");
+        N = Integer.parseInt(l[0]);
 
-        tile = new int[N+1];
-        tile[0] = 1;
-        tile[1] = 1;
+        dp = new int[N + 2];
 
-        for (int i = 2; i < N + 1; i++)
-            tile[i] = -1;
-
-        bw.write(fib(N) + "");
-
-        br.close();
-        bw.flush();
-        bw.close();
-    }
-
-    public static int fib(int N) {
-        if (tile[N] == -1)
-            tile[N] = (fib(N-1) + fib(N-2)) % 15746;
-        return tile[N];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= N; i++) dp[i] = (dp[i - 1] % C + dp[i - 2] % C) % C;
+        System.out.println(dp[N]);
     }
 }
