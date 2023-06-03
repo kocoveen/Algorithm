5c744a6c46f197d6499da15221f0179b746f1441
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-06-03 23:44:12 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 18224 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,29 +1,22 @@
 import java.io.*;
 
 public class Main {
+    static char[] A, B;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char[] A = br.readLine().toCharArray();
-        char[] B = br.readLine().toCharArray();
+        A = br.readLine().toCharArray();
+        B = br.readLine().toCharArray();
 
-        int A_length = A.length;
-        int B_length = B.length;
+        dp = new int[A.length + 1][B.length + 1];
 
-        int[][] dp = new int[A_length+ 1][B_length + 1];
+        for (int i = 1; i <= A.length; i++) 
+            for (int j = 1; j <= B.length; j++)
+                if (A[i - 1] == B[j - 1]) dp[i][j] = dp[i - 1][j - 1] +  1;
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
 
-        for (int i = 1; i <= A_length; i++) {
-            for (int j = 1; j <= B_length; j++) {
-
-                if (A[i - 1] == B[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                else
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-
-            }
-        }
-
-        System.out.println(dp[A_length][B_length]);
+        System.out.println(dp[A.length][B.length]);
     }
 }
