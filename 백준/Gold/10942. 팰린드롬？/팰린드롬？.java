5e04e554a99ee3461834ab387383a7b9b1ebdaee
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-06-04 20:16:22 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1012 ms, Memory: 304324 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -23,9 +23,9 @@ public class Main {
         for (int i = 1; i < N; i++) if (arr[i] == arr[i + 1]) dp[i][i + 1] = 1;
 
         // length n;
-        for (int l = 3; l <= N; l++) 
-            for (int s = 1; s <= N - l + 1; s++) 
-                if (arr[s] == arr[s + l - 1] && dp[s + 1][s + l - 2] == 1)
+        for (int l = 3; l <= N; l++) // 길이가 l 일때
+            for (int s = 1; s <= N - l + 1; s++) // 시작 인덱스 s, 마지막 인덱스 e = s + l - 1;
+                if (arr[s] == arr[s + l - 1] && dp[s + 1][s + l - 2] == 1) // s와 e가 같고, s+1 부터 e-1 까지 팰린드롬인 경우
                     dp[s][s + l - 1] = 1;
 
         M = Integer.parseInt(br.readLine());
@@ -35,8 +35,6 @@ public class Main {
             int e = Integer.parseInt(line[1]);
             sb.append(dp[s][e]).append('\n');
         }
-
         System.out.print(sb);
-
     }
 }
