90429b54d5ff8c07d34a4ed6d5bb67ac2de2c89f
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-06-04 20:36:33 +0900

    [Silver V] Title: 돌 게임, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,15 +1,11 @@
 import java.io.*;
 
 public class Main {
-
     static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
-
-        if (N % 2 != 0) System.out.println("SK");
-        else System.out.println("CY");
+        System.out.println(N % 2 != 0 ? "SK" : "CY");
     }
 }
