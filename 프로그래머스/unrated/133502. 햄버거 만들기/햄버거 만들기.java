39ff55ac2069c7683b85379d5b60c05078885eef
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-06-07 21:01:36 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,29 +1,16 @@
-import java.util.*;
-
 class Solution {
-    static int cnt;
-    
     public int solution(int[] ingredient) {
-        Stack<Integer> S = new Stack();
-        
+        int answer = 0;
+        int[] S = new int[ingredient.length];
+        int top = 0;
         for(int i : ingredient) {
-            S.push(i);
-            if (S.size() < 4) continue;
-            if (isBurger(S)) {
-                cnt++;
-                S.pop();
-                S.pop();
-                S.pop();
-                S.pop();
+            S[top++] = i;
+            if (top < 4) continue;
+            if (S[top - 1] == 1 && S[top - 2] == 3 && S[top - 3] == 2 && S[top - 4] == 1) {
+                top -= 4;
+                answer++;
             }
         }
-        return cnt;
-    }
-    
-    public boolean isBurger(Stack<Integer> S) {
-        return S.elementAt(S.size() - 1) == 1 &&
-            S.elementAt(S.size() - 2) == 3 &&
-            S.elementAt(S.size() - 3) == 2 &&
-            S.elementAt(S.size() - 4) == 1;
+        return answer;
     }
 }
