8b3f13505498159c21038aa20ccfec5cb03f35a6
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-06-22 00:34:25 +0900

    [Bronze II] Title: 벌집, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,17 +1,17 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int room = 0;
-        while(n > 0) {
-            if (room == 0)
-                n -= 1;
-            n -= room * 6;
-            room++;
-        }
-        System.out.println(room);
+    static int t, r, a = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        t = Integer.parseInt(br.readLine());
+
+        do {
+            a += 6 * r;
+            r++;
+        } while (a < t);
+        System.out.println(r);
     }
 }
