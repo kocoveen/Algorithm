bacbe23562317252f7f84c939e48c2dfb14eb37c
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-06-29 17:02:54 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1584 ms, Memory: 139400 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,37 +1,52 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
+    static String[] l;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer sb = new StringBuffer();
-        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        Map<Integer, Integer> cards = new HashMap<>();
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int key = Integer.parseInt(st.nextToken());
-            if (!cards.containsKey(key)) {
-                cards.put(key, 1);
-            } else {
-                int value = cards.get(key);
-                cards.replace(key, value + 1);
-            }
-        }
+        int[] A = new int[N];
+        l = br.readLine().split(" ");
+        for (int i = 0; i < N; i++)
+            A[i] = Integer.parseInt(l[i]);
 
         int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            Integer I = Integer.valueOf(st.nextToken());
-            sb.append(cards.getOrDefault(I, 0)).append(" ");
-        }
+        int[] B = new int[M];
+        l = br.readLine().split(" ");
+        for (int i = 0; i < M; i++)
+            B[i] = Integer.parseInt(l[i]);
 
-        bw.write(sb + "");
+        Arrays.sort(A);
 
-        bw.flush();
-        br.close();
-        bw.close();
+        for (int i = 0; i < l.length; i++)
+            sb.append(upperBound(A, B[i]) - lowerBound(A, B[i]) + 1).append(' ');
+
+        System.out.println(sb);
     }
+
+    private static int lowerBound(int[] A, int target) {
+        int l = 0, r = A.length, m = (l + r) / 2;
+        while (l < r) {
+            if (A[m] < target) l = m + 1;
+            else if (target <= A[m]) r = m;
+            m = (l + r) / 2;
+        }
+        return m;
+    }
+
+    private static int upperBound(int[] A, int target) {
+        int l = 0, r = A.length, m = (l + r) / 2;
+        while (l < r) {
+            if (A[m] <= target) l = m + 1;
+            else if (target < A[m]) r = m;
+            m = (l + r) / 2;
+        }
+        return m - 1;
+    }
+
+
 }
