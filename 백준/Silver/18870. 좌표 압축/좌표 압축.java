dc0dfd088d1010721115449f24cafa77a2641d01
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-06-29 18:08:53 +0900

    [Silver II] Title: 좌표 압축, Time: 1692 ms, Memory: 182160 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,33 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static String[] l;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        int[] A = new int[N];
-        int[] B = new int[N];
-        Map<Integer, Integer> map = new HashMap<>();
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N], B = new int[N], C = new int[N];
+        l = br.readLine().split(" ");
 
         for (int i = 0; i < N; i++) {
-            A[i] = B[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(B);
-        int i = 0;
-        for (int b : B) {
-            if(!map.containsKey(b))
-                map.put(b, i++);
+            A[i] = C[i] = Integer.parseInt(l[i]);
         }
 
-        for (int a : A) {
-            bw.write(map.get(a) + " ");
+        Arrays.sort(A);
+
+        int j = 0;
+        for (int i = 0; i < N; i++) {
+            if (i != 0 && A[i - 1] == A[i]) continue;
+            B[j++] = A[i];
         }
 
-        bw.flush();
-        br.close();
-        bw.close();
+        for (int i : C)
+            sb.append(binarySearch(B, i, j)).append(' ');
+
+        System.out.println(sb);
+    }
+
+    private static int binarySearch(int[] B, int target, int length) {
+        int l = 0, r = length, m = (l + r) / 2;
+        while (l < r) {
+            if (B[m] == target) return m;
+            else if (B[m] < target) l = m + 1;
+            else if (target < B[m]) r = m;
+            m = (l + r) / 2;
+        }
+        return -1;
     }
 }
