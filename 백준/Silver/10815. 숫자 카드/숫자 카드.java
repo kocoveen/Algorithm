8b9ee5a46c5c659c496329037f694fc471f9f60e
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-06-29 20:18:32 +0900

    [Silver V] Title: 숫자 카드, Time: 1272 ms, Memory: 123612 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,42 +1,38 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
+    static String[] l;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        int[] A = new int[N];
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        int[] cards = new int[n];
+        l = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            cards[i] = Integer.parseInt(l[i]);
         }
-        Arrays.sort(A);
+        Arrays.sort(cards);
 
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int key = Integer.parseInt(st.nextToken());
-            bw.write(binarySearch(A, key) + " ");
+        int m = Integer.parseInt(br.readLine());
+        l = br.readLine().split(" ");
+        for (int i = 0; i < m; i++) {
+            int target = Integer.parseInt(l[i]);
+            sb.append(binarySearch(cards, target)).append(' ');
         }
+        System.out.println(sb);
 
-        bw.flush();
-        br.close();
-        bw.close();
     }
 
-    private static int binarySearch(int[] A, int key) {
-        int low = 0, high = A.length-1, mid;
-        while (low <= high) {
-            mid = (low + high) / 2;
-            if (A[mid] == key)
-                return 1;
-            else if (A[mid] < key)
-                low = mid + 1;
-            else
-                high = mid - 1;
+    private static long binarySearch(int[] cards, int target) {
+        int l = 0, r = cards.length, m = (l + r) / 2;
+        while (l < r) {
+            if (target == cards[m]) return 1;
+            else if (cards[m] < target) l = m + 1;
+            else r = m;
+            m = (l + r) / 2;
         }
         return 0;
     }
