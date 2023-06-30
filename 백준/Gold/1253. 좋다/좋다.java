13612d05b2f9af31bcde87bee68ccc9365048f33
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-06-30 21:50:57 +0900

    [Gold IV] Title: 좋다, Time: 320 ms, Memory: 14768 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -3,36 +3,38 @@ import java.util.Arrays;
 
 public class Main {
     static String[] l;
+    static int[] A;
+    static int cnt, n;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] A = new int[n];
-        boolean[] chk = new boolean[n];
+        n = Integer.parseInt(br.readLine());
+        A = new int[n];
         l = br.readLine().split(" ");
         for (int i = 0; i < n; i++)
             A[i] = Integer.parseInt(l[i]);
 
         Arrays.sort(A);
 
-        int cnt = 0;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int target = A[i] + A[j];
-                int l = lowerBound(A, target);
+        for (int i = 0; i < n; i++) solve(i);
+        System.out.println(cnt);
+    }
 
-                if (l >= n) continue;
-                for (int k = l; k < n; k++) {
-                    if (k == i || k == j || chk[k] || A[k] != target) continue;
-                    chk[k] = true;
+    private static void solve(int i) {
+        for (int j = 0; j < n; j++) {
+            if(j == i) continue;
+            int target = A[i] - A[j];
+            int idx = lowerBound(A, target);
+
+            while (idx < n && A[idx] == target) {
+                if (idx != i && idx != j) {
+                    cnt++;
+                    return;
                 }
+                idx++;
             }
         }
-
-        for (int i = 0; i < n; i++)
-            if (chk[i]) cnt++;
-        System.out.println(cnt);
     }
 
     private static int lowerBound(int[] A, int target) {
@@ -43,15 +45,5 @@ public class Main {
             m = (l + r) / 2;
         }
         return m;
-    }
-
-    private static int upperBound(int[] A, int target) {
-        int l = 0, r = A.length, m = (l + r) / 2;
-        while (l < r) {
-            if (target < A[m]) r = m;
-            else l = m + 1;
-            m = (l + r) / 2;
-        }
-        return m - 1;
     }
 }
