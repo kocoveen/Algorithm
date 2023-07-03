620b7fba3d410dcd3c9a88e926d9cd613758cf8a
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-07-03 22:03:42 +0900

    [Gold IV] Title: 공유기 설치, Time: 312 ms, Memory: 29204 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,51 +1,41 @@
 import java.io.*;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
-    static int min;
-    static int max;
-    static int[] H;
+    static String[] l;
+
+    static long[] ans = new long[3];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
+        l = br.readLine().split(" ");
+        int n = Integer.parseInt(l[0]);
+        int c = Integer.parseInt(l[1]);
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++) A[i] = Integer.parseInt(br.readLine());
 
-        H = new int[N];
-        for (int i = 0; i < N; i++)
-            H[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(A);
 
-        Arrays.sort(H);
-
-        min = 1; max = H[N - 1] - H[0] + 1;
-
-        while (min < max) {
-            int mid = (min + max) / 2;
-            if (isPossible(mid) < C)
-                max = mid;
-            else
-                min = mid + 1;
+        int l = 1, r = A[n - 1] - A[0] + 1;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (canInstall(A, m) < c) r = m;
+            else l = m + 1;
         }
-
-        System.out.println(min - 1);
+        System.out.println(l - 1);
     }
 
-    private static int isPossible(int d) {
-        int count = 1;
-        int lastLoc = H[0];
+    // 설치 할 두 집 사이의 거리가 m 일 때, 설치 할 공유기의 수를 구하는 함수
+    private static int canInstall(int[] A, int m) {
+        int cnt = 1;
+        int lastLoc = A[0];
 
-        for (int loc : H) {
-            if (loc - lastLoc >= d) {
-                count++;
-                lastLoc = loc;
-            }
+        for (int Loc : A) {
+            if (Loc - lastLoc < m) continue;
+            cnt++;
+            lastLoc = Loc;
         }
-        return count;
+        return cnt;
     }
 }
