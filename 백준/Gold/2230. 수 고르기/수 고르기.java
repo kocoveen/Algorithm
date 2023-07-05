03db4ebdcc6f39314672b5cf2cb1c083c87cd584
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-07-05 19:04:39 +0900

    [Gold V] Title: 수 고르기, Time: 380 ms, Memory: 28104 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -13,26 +13,14 @@ public class Main {
         int m = Integer.parseInt(l[1]);
         A = new int[n];
         for (int i = 0; i < n; i++) A[i] = Integer.parseInt(br.readLine());
-
         Arrays.sort(A);
 
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < n - 1; i++) {
-            int idx = lowerBound(A, i, m);
-            if (idx == - 1) continue;
-            min = Math.min(min, Math.abs(A[idx] - A[i]));
+        int en = 0, min = Integer.MAX_VALUE;
+        for (int st = 0; st < n; st++) {
+            while (en < n && A[en] - A[st] < m) en++;
+            if (en == n) break;
+            min = Math.min(min, A[en] - A[st]);
         }
         System.out.println(min);
     }
-
-    private static int lowerBound(int[] A, int st, int target) {
-        int l = st + 1, r = A.length, m = (l + r) / 2;
-        while (l < r) {
-            if (A[m] - A[st] < target) l = m + 1;
-            else r = m;
-            m = (l + r) / 2;
-        }
-        return (m >= A.length ? -1 : m);
-    }
-
 }
