b0e07fd543c06a986b36f3f8ffd1846662119bdb
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-08-16 20:23:02 +0900

    [level 2] Title: 타겟 넘버, Time: 0.34 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,50 +1,16 @@
 class Solution {
-    static int count;
-    static int[] staticNums;
-    static int[] minus;
-        
     public int solution(int[] numbers, int target) {
         int answer = 0;
-        
-        staticNums = numbers;
-        minus = new int[numbers.length];
-        
-        for (int i = 0; i < minus.length; i++) {
-            minus[i] = 1;
-        }
-        
-        for (int depth = 0; depth < numbers.length; depth++) {
-            func(0, depth, 0, target);
-        }
-        
-        return count;
+        answer = dfs(numbers, 0, 0, target);
+        return answer;
     }
-    
-    private void func(int st, int depth, int mCount, int target) {
-        if (mCount == depth) {
-            if (productSum(staticNums, minus) == target) {
-                count++;
+    int dfs(int[] numbers, int n, int sum, int target) {
+        if(n == numbers.length) {
+            if(sum == target) {
+                return 1;
             }
-            return;
+            return 0;
         }
-        
-        for (int i = st; i < staticNums.length; i++) {
-            if (minus[i] == -1) continue;
-            minus[i] = -1;
-            func(i + 1, depth, mCount + 1, target);
-            minus[i] = 1;
-        }
+        return dfs(numbers, n + 1, sum + numbers[n], target) + dfs(numbers, n + 1, sum - numbers[n], target);
     }
-    
-    private int productSum(int[] numbers, int[] minus) {
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i] * minus[i];
-        }
-        return sum;
-    }
-    
-//     for (int i : staticNums) {
-//         System.out.println(i);
-//     }
 }
