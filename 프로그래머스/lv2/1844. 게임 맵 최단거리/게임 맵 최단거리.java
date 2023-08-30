8b9cfe32dc750b620c626b121bc7c5d25fbd901c
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-08-30 14:22:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.37 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Solution {
+    
+    static int[] dr = {1, 0, -1, 0};
+    static int[] dc = {0, -1, 0, 1};
+    static int n, m;
+    
+    public int solution(int[][] maps) {
+        n = maps.length;
+        m = maps[0].length;
+        
+        boolean[][] visited = new boolean[n][m];
+        int[][] board = new int[n][m];
+        Queue<Pair> Q = new LinkedList<>();
+        
+        Q.add(new Pair(0, 0));
+        visited[0][0] = true;
+        board[0][0] = 1;
+        
+        while (!Q.isEmpty()) {
+            Pair p = Q.remove();
+            for (int i = 0; i < 4; i++) {
+                int nr = p.r + dr[i];
+                int nc = p.c + dc[i];
+                if (isNotBoundaryIn(nr, nc)) continue;
+                if (maps[nr][nc] == 0 || visited[nr][nc]) continue;
+                visited[nr][nc] = true;
+                board[nr][nc] = board[p.r][p.c] + 1;
+                Q.add(new Pair(nr, nc));
+            }
+        }
+        
+        // for (int[] i : board) {
+        //     for (int j : i) {
+        //         System.out.printf("%d ", j);
+        //     }
+        //     System.out.println();
+        // }
+        
+        int answer = board[n - 1][m - 1];
+        return answer == 0 ? -1 : answer;
+    }
+    
+    private static boolean isNotBoundaryIn(int nr, int nc) {
+        return nr >= n || nc >= m || nr < 0 || nc < 0; 
+    }
+    
+    private static class Pair {
+        int r;
+        int c;
+        
+        public Pair(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+}
