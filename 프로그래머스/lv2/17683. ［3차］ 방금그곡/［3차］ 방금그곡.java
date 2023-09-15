52f2c72ba6d3bb42e738acf5b1d8226df508cb31
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-09-15 22:15:15 +0900

    [level 2] Title: [3차] 방금그곡, Time: 7.73 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,97 +1,46 @@
-import java.util.*;
-import java.util.regex.MatchResult;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
 class Solution {
-    private static final String PATTERN = "([A-G]#?)";
-    private static final String CODE = "C, C#, D, D#, E, F, F#, G, G#, A, A#, B";
-    static Map<String, String> map = new HashMap<>();
-    static Pattern regex = Pattern.compile(PATTERN);
-
     public String solution(String m, String[] musicinfos) {
-        initMap(map);
+        String answer = "(None)";
+        int time = 0;
 
-        Spliter spliter = new Spliter();
-        String origin = String.join("", spliter.splitNotes(m).stream()
-                .map(note -> map.get(note))
-                .collect(Collectors.toList()));
+        m = edit(m);
 
-        List<Music> musics = Arrays.stream(musicinfos)
-                .map(info -> info.split(","))
-                .map(info -> new Music(info[0], info[1], info[2], info[3]))
-                .collect(Collectors.toList());
+        for (int inx = 0; inx < musicinfos.length; inx++) {
 
-        List<Music> matchList = musics.stream()
-                .filter(music -> music.notes.contains(origin))
-                .sorted(Comparator.comparingInt(music -> -music.playTime))
-                .collect(Collectors.toList());
+            String[] info = musicinfos[inx].split(",");
 
-        if (matchList.isEmpty()) {
-            return "(None)";
-        }
+            int start = timeToDigit(info[0]);
+            int end = timeToDigit(info[1]);
+            int t = end - start;
 
-        return matchList.get(0).name;
-    }
-
-    private void initMap(Map<String, String> map) {
-        String[] codes = CODE.split(", ");
-        map.put(codes[0], "C");
-        map.put(codes[1], "H");
-        map.put(codes[2], "D");
-        map.put(codes[3], "I");
-        map.put(codes[4], "E");
-        map.put(codes[5], "F");
-        map.put(codes[6], "J");
-        map.put(codes[7], "G");
-        map.put(codes[8], "K");
-        map.put(codes[9], "A");
-        map.put(codes[10], "L");
-        map.put(codes[11], "B");
-    }
-
-    static class Music {
-        int playTime;
-        String name;
-        String notes;
-
-        public Music(String startTime, String endTime, String name, String notes) {
-            this.playTime = timeToDigit(endTime) - timeToDigit(startTime);
-            this.name = name;
-            this.notes = extractedNotes(notes, playTime);
-        }
-
-        private String extractedNotes(String notes, int playTime) {
-            Matcher matcher = regex.matcher(notes);
-
-            List<String> noteList = matcher.results()
-                    .map(MatchResult::group)
-                    .map(code -> map.get(code))
-                    .collect(Collectors.toList());
-
-            List<String> newList = new ArrayList<>();
-            int repeat = (playTime - 1) / noteList.size() + 1;
-            for (int i = 0; i < repeat; i++) {
-                newList.addAll(noteList);
+            if (t > time) {
+                info[3] = edit(info[3]);
+                StringBuilder sb = new StringBuilder();
+                for (int jnx = 0; jnx < t; jnx++) {
+                    sb.append(info[3].charAt(jnx % (info[3].length())));
+                }
+                if (sb.toString().contains(m)) {
+                    answer = info[2];
+                    time = t;
+                }
             }
-
-            return String.join("", newList.subList(0, playTime));
         }
 
-        private int timeToDigit(String time) {
-            String[] times = time.split(":");
-            return Integer.parseInt(times[0]) * 60 + Integer.parseInt(times[1]);
-        }
+        return answer;
     }
 
-    static class Spliter {
-        public List<String> splitNotes(String notes) {
-            Matcher matcher = regex.matcher(notes);
 
-            return matcher.results()
-                    .map(MatchResult::group)
-                    .collect(Collectors.toList());
-        }
+    public int timeToDigit(String time) {
+        String[] times = time.split(":");
+        return Integer.parseInt(times[0]) * 60 + Integer.parseInt(times[1]);
+    }
+
+    public String edit(String m) {
+        m = m.replaceAll("C#", "V");
+        m = m.replaceAll("D#", "W");
+        m = m.replaceAll("F#", "X");
+        m = m.replaceAll("G#", "Y");
+        m = m.replaceAll("A#", "Z");
+        return m;
     }
 }
