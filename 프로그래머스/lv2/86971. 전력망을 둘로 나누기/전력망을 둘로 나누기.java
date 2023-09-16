f1ef8119061c3e6c64c3098d06aaecb5bf0297fc
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-09-17 00:27:54 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.26 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,43 +1,29 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-
-        List<List<Integer>> tree = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            tree.add(new ArrayList<>());
-        }
-
-        Arrays.stream(wires).forEach(wire -> {
-            tree.get(wire[0]).add(wire[1]);
-            tree.get(wire[1]).add(wire[0]);
-        });
-
-        for (int[] wire : wires) {
-            int network1 = bfs(wire[0], wire[1], new boolean[n + 1], tree);
-            int network2 = bfs(wire[1], wire[0], new boolean[n + 1], tree);
-            answer = Math.min(Math.abs(network1 - network2), answer);
-        }
-
-        return answer;
-    }
-
-    private int bfs(int node1, int node2, boolean[] visited, List<List<Integer>> tree) {
-        Queue<Integer> Q = new LinkedList<>();
-        int count = 0;
-        Q.add(node1);
-        visited[node1] = true;
-
-        while (!Q.isEmpty()) {
-            int cur = Q.remove();
-            count++;
-            for (int node : tree.get(cur)) {
-                if (visited[node] || node == node2) continue;
-                Q.add(node);
-                visited[node] = true;
+    int N, min;
+    int[][] map;
+    int[] vst;
+    int dfs(int n){
+        vst[n] = 1;
+        int child = 1;
+        for(int i = 1; i <= N; i++) {
+            if(vst[i] == 0 && map[n][i] == 1) {
+                vst[i] = 1;
+                child += dfs(i);
             }
         }
-        return count;
+        min = Math.min(min, Math.abs(child - (N - child)));
+        return child;
+    }
+    public int solution(int n, int[][] wires) {
+        N = n;
+        min = n;
+        map = new int[n+1][n+1];
+        vst = new int[n+1];
+        for(int[] wire : wires) {
+            int a = wire[0], b = wire[1];
+            map[a][b] = map[b][a] = 1;
+        }
+        dfs(1);
+        return min;
     }
 }
