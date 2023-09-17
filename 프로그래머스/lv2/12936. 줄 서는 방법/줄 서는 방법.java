a6aa5fa82eebe1dbd7bf096dee7ba97525edad67
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-09-18 00:30:15 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.06 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, long k) {
+        List<Integer> mens = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            mens.add(i + 1);
+        }
+        int[] answer = new int[mens.size()];
+
+        long fac = factorial(mens.size());
+        int turn = 0;
+        while (!mens.isEmpty()) {
+            fac /= mens.size();
+            long idx = (k - 1) / fac;
+            answer[turn++] = mens.remove((int) idx);
+            k = (k - 1) % fac + 1;
+        }
+
+        return answer;
+    }
+
+    private long factorial(int f) {
+        long t = 1;
+        for (int i = 2; i <= f; i++) {
+            t *= i;
+        }
+        return t;
+    }
+}
