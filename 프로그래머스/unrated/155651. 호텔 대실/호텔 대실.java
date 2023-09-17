ceb2bc1b72e8026b48be70739eae091cfdc69930
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-09-17 16:05:54 +0900

    [unrated] Title: 호텔 대실, Time: 6.07 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.java
@@ -1,53 +1,36 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 class Solution {
     public int solution(String[][] book_time) {
 
-        List<Reservation> reservations = new ArrayList<>();
-        for (String[] book : book_time) {
-            reservations.add(new Reservation(book));
-        }
-        reservations.sort(Comparator.comparingInt(r -> r.start_time));
+        int[][] bkt = new int[book_time.length][];
 
-        List<Stack<Reservation>> rent = new ArrayList<>();
-
-        for (Reservation r : reservations) {
-            if (rent.isEmpty()) {
-                rent.add(new Stack<>());
-            }
-
-            boolean flag = false;
-            for (Stack<Reservation> room : rent) {
-                if (room.isEmpty() || r.start_time >= room.peek().end_time) {
-                    room.push(r);
-                    flag = true;
-                    break;
-                }
-            }
-
-            if (!flag) {
-                Stack<Reservation> S = new Stack<>();
-                S.push(r);
-                rent.add(S);
-            }
+        for (int i = 0; i < book_time.length; i++) {
+            bkt[i] = new int[] { parseTime(book_time[i][0]), parseTime(book_time[i][1]) + 10 };
         }
 
-        return rent.size();
+        Arrays.sort(bkt, (a, b) -> a[0] - b[0]);
+
+        PriorityQueue<int[]> que = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        int ans = 0;
+
+        for (int i = 0; i < bkt.length; i++) {
+            while (!que.isEmpty() && que.peek()[1] <= bkt[i][0]) {
+                que.poll();
+            }
+            que.add(bkt[i]);
+            ans = Math.max(ans, que.size());
+        }
+
+        return ans;
     }
 
-    static class Reservation {
-        int start_time;
-        int end_time;
+    protected int parseTime(String time) {
 
-        public Reservation (String[] book_time) {
-            this.start_time = timeToDigit(book_time[0]);
-            this.end_time = timeToDigit(book_time[1]) + 10;
-        }
-
-        private int timeToDigit(String time) {
-            String[] splits = time.split(":");
-            return Integer.parseInt(splits[0]) * 60 + Integer.parseInt(splits[1]);
-        }
+        String[] hhmm = time.split(":");
+        int hour = Integer.parseInt(hhmm[0]), min = Integer.parseInt(hhmm[1]);
+        return hour * 60 + min;
 
     }
 }
