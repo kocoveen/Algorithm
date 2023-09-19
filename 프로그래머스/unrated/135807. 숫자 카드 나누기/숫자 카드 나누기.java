ed8cc8b08643e9a23b65376020b29b58f44b0ad0
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-09-19 12:21:14 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 1.74 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,7 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
-        int answer = 0;
-
         int A = arrayA[0];
         for (int i = 1; i < arrayA.length; i++) {
             A = gcd(A, arrayA[i]);
@@ -27,21 +27,7 @@ class Solution {
     }
 
     private boolean indivisible(int[] array, int gcd) {
-        for (int i : array) {
-            if (i >= gcd && i % gcd == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean containingGcd(int[] array, int gcd) {
-        for (int i : array) {
-            if (i == gcd) {
-                return true;
-            }
-        }
-        return false;
+        return Arrays.stream(array).noneMatch(i -> i >= gcd && i % gcd == 0);
     }
 
     private int gcd(int a, int b) {
