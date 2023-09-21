83b3d6425c62b5c4f101f4d331b11804f3cf2d88
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-09-21 16:01:25 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.10 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,80 +1,48 @@
-import java.util.*;
-
 class Solution {
-    static int[] dr = {1, 0, -1, 0};
-    static int[] dc = {0, 1, 0, -1};
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static boolean[][] visit;
 
-    int[][] room;
-    Queue<int[]> Q;
-    List<int[]> L; // P 좌표
+    static int[] answer;
 
-    public int[] solution(String[][] places) {
-        int[] answer = new int[5];
-
-        for (int k = 0; k < 5; k++) {
-            L = new ArrayList<>();
-            String[] place = places[k]; // 대기실
-            boolean valid = true; // 준수 여부
-
-            // 대기실 초기화
-            room = new int[5][5];
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-                    if (place[i].charAt(j) == 'P') {
-                        L.add(new int[]{i, j});
-                    } else if (place[i].charAt(j) == 'X') {
-                        room[i][j] = -1;
-                    }
-                }
-            }
-
-            for (int[] p : L) {
-                if (!valid) break;
-
-                Q = new LinkedList<>();
-                Q.add(new int[]{p[0], p[1]});
-                boolean[][] visited = new boolean[5][5];
-
-                while (!Q.isEmpty() && valid) {
-                    int[] cur = Q.remove();
-                    visited[cur[0]][cur[1]] = true;
-
-                    for (int i = 0; i < 4; i++) {
-                        int nr = cur[0] + dr[i];
-                        int nc = cur[1] + dc[i];
-                        if (nr > 4 || nc > 4 || nr < 0 || nc < 0) continue;
-                        if (room[nr][nc] == -1 || visited[nr][nc]) continue;
-
-                        if (place[nr].charAt(nc) == 'P') {
-                            if (room[cur[0]][cur[1]] + 1 <= 2) {
-                                valid = false;
-                                break;
-                            } else {
-                                continue;
-                            }
-                        }
-
-                        room[nr][nc] = room[cur[0]][cur[1]] + 1;
-                        Q.add(new int[]{nr, nc});
-                    }
-                }
-            }
-
-
-            // for (int[] r : room) {
-            //     for (int i : r) {
-            //         System.out.printf("%2d ", i);
-            //     }
-            //     System.out.println();
-            // }
-            // System.out.println();
-
-            if (valid) answer[k] = 1;
-            else answer[k] = 0;
+    public void dfs(int num, int x, int y, int count, String[] places){
+        if (count > 2) return;
+        if (count > 0 && count <= 2 && places[x].charAt(y) == 'P'){
+            //2칸 범위내에 다른 응시자가 있을 경우 거리두기 미준수로 0처리
+            answer[num] = 0;
+            return;
         }
-
-
-        return answer;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            //배열 범위 밖으로 초과하는지 여부 검사, 파티션으로 분리되어 있는 경우 상관 없음.
+            if (nx >= 0 && nx < 5 && ny >= 0 && ny < 5 && places[nx].charAt(ny) != 'X') {
+                if (visit[nx][ny]) continue; //이미 방문한 곳일 경우 생략
+                visit[nx][ny] = true;
+                dfs(num, nx, ny, count + 1, places);
+                visit[nx][ny] = false;
+            }
+        }
     }
 
+    public int[] solution(String[][] places) {
+        answer = new int[places.length];
+        for (int i = 0; i < places.length; i++) {
+            answer[i] = 1;
+        }
+
+        for (int i = 0; i < places.length; i++) {
+            visit = new boolean[5][5];
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (places[i][j].charAt(k) == 'P'){
+                        visit[j][k] = true;
+                        dfs(i, j, k, 0, places[i]);
+                        visit[j][k] = false;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
 }
