807d89655860b04ab3a5dce74ff7b77ecc81bc45
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-09-27 20:17:57 +0900

    [unrated] Title: 디펜스 게임, Time: 0.83 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
@@ -1,41 +1,23 @@
 import java.util.*;
 
 class Solution {
-
-    TreeMap<Integer, TreeSet<Integer>> map = new TreeMap<>();
-
     public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
-
-        int i, size = 0;
-        for (i = 0; i < enemy.length; i++) {
-
-            map.computeIfAbsent(enemy[i], key -> new TreeSet<>()).add(i);
-            size++;
-
-            if (size > k) {
-                int enemySize = map.firstKey();
-                if (map.get(enemySize).size() == 1) {
-                    map.remove(enemySize);
-                } else {
-                    map.get(enemySize).pollLast();
+        for (int e : enemy) {
+            pq.add(e);
+            answer++;
+            n -= e;
+            if (n < 0) {
+                if(k == 0){
+                    return answer - 1;
                 }
-                n -= enemySize;
-
-                if (n < 0) {
-                    break;
-                }
+                n += pq.poll();
+                k--;
             }
         }
 
-//        for (Map.Entry<Integer, TreeSet<Integer>> en : map.entrySet()) {
-//            System.out.printf("%d : ", en.getKey());
-//            for (Integer i : en.getValue()) {
-//                System.out.printf("[%d]", i);
-//            }
-//            System.out.println();
-//        }
-
-        return i;
+        return enemy.length;
     }
 }
