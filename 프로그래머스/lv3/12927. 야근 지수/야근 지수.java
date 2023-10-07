f9b547fd15093f40020a407daedff8cb3d576943
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-10-07 16:29:55 +0900

    [level 3] Title: 야근 지수, Time: 31.77 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
@@ -4,22 +4,27 @@ class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
         int endIndex = works.length - 1;
-        
+
         Arrays.sort(works);
         for (int i = 0; i < n; i++) {
-            int index = findLowerIndexMaxBacklog(works, works[endIndex]);
-            if (works[index] == 0) {
+            // target(works 중 가장 큰 값)의 lowerBound 찾기            
+            int index = findMaxWorkLowerBound(works, works[endIndex]);
+            
+            // 가장 큰 값이 0 이면 continue;
+            if (works[index] == 0) { 
                 continue;
             }
+            
+            // 가장 큰 값이 0이 아니면 그 값 1 빼기
             works[index]--;
         }
-        
+
+        // 야근 지수 구하기
         answer = evaluateMesure(works);
-        
         return answer;
     }
-    
-    private int findLowerIndexMaxBacklog(int[] works, int target) {
+
+    private int findMaxWorkLowerBound(int[] works, int target) {
         int l = 0, h = works.length, m;
         while (l < h) {
             m = (l + h) / 2;
@@ -31,7 +36,7 @@ class Solution {
         }
         return l;
     }
-    
+
     private long evaluateMesure(int[] works) {
         long measure = 0;
         for (int i = 0; i < works.length; i++) {
