ffb657fa7aa9308ec8e296c7c00ad49cd92bd58a
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-10-09 13:55:02 +0900

    [level 3] Title: 등굣길, Time: 0.48 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.java
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.java
@@ -10,37 +10,19 @@ class Solution {
             route[puddle[0]][puddle[1]] = -1;
         }
         
+        route[1][1] = 1;
+        
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                
-                if (i == 1 && j == 1) {
-                    route[i][j] = 1;
+                if(route[i][j] == -1){
+                    route[i][j] = 0;
                     continue;
                 }
-                
-                if (route[i][j] < 0) {
-                    continue;
-                }
-                
-                if (route[i - 1][j] == -1) {
-                    route[i][j] = route[i][j - 1];
-                } else if (route[i][j - 1] == -1) {
-                    route[i][j] = route[i - 1][j];
-                } else {
-                    route[i][j] = (route[i - 1][j] % C + route[i][j - 1] % C) % C;
-                }
+                if(i != 1) route[i][j] += route[i - 1][j] % C;
+                if(j != 1) route[i][j] += route[i][j - 1] % C;
             }
         }
                 
-        return route[m][n];
-    }
-    
-    private void print(int[][] route) {
-        for (int[] r : route) {
-            for (int i : r) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-        }
+        return route[m][n] % C;
     }
 }
