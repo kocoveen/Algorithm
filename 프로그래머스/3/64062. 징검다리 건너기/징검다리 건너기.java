05d8e7403a0acc276431aedbc14072a2d5352d36
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-10-12 14:06:54 +0900

    [level 3] Title: 징검다리 건너기, Time: 42.51 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,36 +1,29 @@
 class Solution {
     public int solution(int[] stones, int k) {
-        int l = 0, r = findMaxStone(stones);
+        int l = 0, r = 200000000;
         while (l < r) {
             int m = (l + r) / 2;
-            if (findMaxDistance(stones, m) >= k) {
-                r = m;
-            } else {
+            if (canCross(stones, m, k)) {
                 l = m + 1;
+            } else {
+                r = m;
             }
         }
         return l;
     }
     
-    private int findMaxDistance(int[] stones, int k) {
-        int mx = 0;
+    private boolean canCross(int[] stones, int m, int k) {
         int count = 0;
         for (int stone : stones) {
-            if (stone <= k) {
+            if (stone <= m) {
                 count++;
+                if (count == k) {
+                    return false;
+                }
             } else {
                 count = 0;
             }
-            mx = Math.max(mx, count);
         }
-        return mx;
-    }
-    
-    private int findMaxStone(int[] stones) {
-        int mx = 0;
-        for (int stone : stones) {
-            mx = Math.max(mx, stone);
-        }
-        return mx;
+        return true;
     }
 }
