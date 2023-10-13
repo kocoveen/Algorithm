f559b45c3ec603d62e7ae5ef66a59ba1511a0c2b
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-10-13 19:35:38 +0900

    [level 3] Title: 섬 연결하기, Time: 0.53 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int n, int[][] costs) {
+        
+        Arrays.sort(costs, (c1, c2) -> c1[2] - c2[2]);
+        int[] parent = makeParentInit(n);
+        
+        return kruskal(costs, parent);
+    }
+    
+    private int kruskal(int[][] costs, int[] parent) {
+        int sum = 0;        
+        for (int i = 0; i < costs.length; i++) {
+            int root1 = find(parent, costs[i][0]);
+            int root2 = find(parent, costs[i][1]);
+
+            if (root1 != root2) {
+                union(parent, root1, root2);
+                sum += costs[i][2];
+            }
+        }
+        return sum;
+    }
+    
+    private int find(int[] parent, int index) {
+        if (parent[index] < 0) {
+            return index;
+        }
+        
+        // while(parent[index] != -1) {
+        //     index = parent[index];
+        // }
+        // return index;
+        return find(parent, parent[index]);
+    }
+    
+    private void union(int[] parent, int root1, int root2) {
+        int r1 = find(parent, root1);
+        int r2 = find(parent, root2);
+        
+        if (r1 != r2) {
+            parent[r2] = r1;
+        }
+    }
+    
+    private int[] makeParentInit(int n) {
+        int[] parent = new int[n];
+        Arrays.fill(parent, -1);
+        return parent;
+    }
+}
