247ac7ffcf5878cedb94619e654612afafb1ef56
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-11-06 00:44:28 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,79 +1,50 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
-    static boolean[][] graph;
-    static int n;
-    static int m;
-    static int cnt = 0;
+// 파이프 옮기기 1
+public class Main {
+    private static int n;
+    private static int[][] map;
 
-    static void moveHorizontal(int r, int c) {
-        if (isArrived(r, c)) {
-            cnt++;
-            return;
-        }
+    private static int solution(){
+        int answer = 0;
 
-        int nxt_r = r + 1;
-        int nxt_c = c + 1;
-        if (nxt_c < n && !graph[r][nxt_c]) {
-            moveHorizontal(r, nxt_c);
-            if (nxt_r < n && !graph[nxt_r][nxt_c] && !graph[nxt_r][c]){
-                moveDiagonal(nxt_r, nxt_c);
+        int[][][] dp = new int[n+1][n+1][3];
+        dp[1][2][0] = 1;
+
+        for(int r=1; r<=n; r++){
+            for(int c=1; c<=n; c++){
+                if(r==1 && c==2){
+                    continue;
+                }
+                // 가로
+                dp[r][c][0] = (map[r][c] != 1) ? dp[r][c-1][0] + dp[r][c-1][2] : 0;
+                // 세로
+                dp[r][c][1] = (map[r][c] != 1) ? dp[r-1][c][1] + dp[r-1][c][2] : 0;
+                // 대각선
+                dp[r][c][2] = (map[r][c] != 1 && map[r][c-1] == 0 && map[r-1][c] == 0) ?
+                        dp[r-1][c-1][0] + dp[r-1][c-1][1] + dp[r-1][c-1][2] : 0;
             }
         }
+
+        answer = dp[n][n][0] + dp[n][n][1] + dp[n][n][2];
+        return answer;
     }
 
-    static void moveDiagonal(int r, int c) {
-        if (isArrived(r, c)) {
-            cnt++;
-            return;
-        }
-
-        int nxt_r = r + 1;
-        int nxt_c = c + 1;
-        if (nxt_c < n && !graph[r][nxt_c]) {
-            moveHorizontal(r, nxt_c);
-            if (nxt_r < n && !graph[nxt_r][nxt_c] && !graph[nxt_r][c]){
-                moveDiagonal(nxt_r, nxt_c);
-            }
-        }
-        if (nxt_r < n && !graph[nxt_r][c]) moveVertical(nxt_r, c);
-    }
-
-    static void moveVertical(int r, int c) {
-        if (isArrived(r, c)) {
-            cnt++;
-            return;
-        }
-        int nxt_r = r + 1;
-        int nxt_c = c + 1;
-        if (nxt_r < n && !graph[nxt_r][c]) {
-            moveVertical(nxt_r, c);
-            if (nxt_c < n && !graph[nxt_r][nxt_c] && !graph[r][nxt_c]){
-                moveDiagonal(nxt_r, nxt_c);
-            }
-        }
-    }
-
-    static boolean isArrived(int r, int c) {
-        return r == m && c == m;
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        m = n-1;
-        graph = new boolean[n][n];
+        map = new int[n+1][n+1];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (br.read() == '1') {
-                    graph[i][j] = true;
-                }
-                br.read();
+        for(int i=1; i<=n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=1; j<=n; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        moveHorizontal(0, 1);
-        System.out.println(cnt);
+        System.out.println(solution());
     }
 }
