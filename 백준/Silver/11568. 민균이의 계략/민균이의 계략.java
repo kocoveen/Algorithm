a0e7fc240f6fdb71493dc17067564a3709e0a051
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-11-10 17:55:06 +0900

    [Silver II] Title: 민균이의 계략, Time: 136 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/11568. 민균이의 계략/민균이의 계략.java
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.java
@@ -2,32 +2,45 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    static int[] memo;
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        int[] dp = new int[n];
 
-        String[] split = br.readLine().split(" ");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(split[i]);
+        String[] st = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st[i]);
         }
 
-        dp[0] = 1;
-        for (int i = 1; i < n; i++) {
-            dp[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+        memo = new int[n + 1];
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            if (memo[len] < arr[i]) { // memo의 마지막보다 arr[i]가 크면
+                // memo 마지막에 arr[i] 추가
+                len += 1;
+                memo[len] = arr[i];
+            } else { // memo의 마지막보다 arr[i]가 작으면
+                // memo에 arr[i]가 들어갈 index를 찾음 (memo는 오름차순이라 이진탐색이 가능)
+                // arr[i] 보다 작은 memo 원소 중, 가장 큰 곳인 j에 집어넣음. -> Lower Bound로 찾기
+                int idx = binarySearch(0, len, arr[i]); 
+                memo[idx] = arr[i];
             }
         }
+        System.out.println(len);
+    }
 
-        int ans = 0;
-        for (int i : dp) {
-            ans = Math.max(ans, i);
+    // Lower bound
+    static int binarySearch(int l, int r, int target) {
+        while(l < r) {
+            int m = (l + r) / 2;
+            if (memo[m] < target) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
         }
-
-        System.out.println(ans);
+        return r;
     }
 }
