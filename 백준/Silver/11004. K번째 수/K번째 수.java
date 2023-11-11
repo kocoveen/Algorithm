7a27c2c330d6356d7e0594120f08694706607dab
Dumbbell00 <79296295+dumbbellpullover@users.noreply.github.com>
2023-11-11 14:47:52 +0900

    [Silver V] Title: K번째 수, Time: 1696 ms, Memory: 370068 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,23 +1,56 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/**
+ * 퀵정렬 자바 버전
+ * pivot = arr[(L + R) / 2];
+ */
 
 public class Main {
 
-    static String[] st;
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = br.readLine().split(" ");
-        int n = Integer.parseInt(st[0]);
-        int k = Integer.parseInt(st[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        st = br.readLine().split(" ");
+        st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
+
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st[i]);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
+        quickSort(arr, 0, n - 1);
         System.out.println(arr[k - 1]);
+    }
+
+    private static void quickSort(int[] arr, int l, int r) {
+        int p = partition(arr, l, r);
+        if (l < p - 1)
+            quickSort(arr, l, p - 1);
+        if (p < r)
+            quickSort(arr, p, r);
+    }
+
+    private static int partition(int[] arr, int l, int r) {
+        int pivot = arr[(l + r) / 2];
+        while (l <= r) {
+            while (arr[l] < pivot) l++;
+            while (arr[r] > pivot) r--;
+            if (l <= r) {
+                swap (arr, l, r);
+                l++;
+                r--;
+            }
+        }
+        return l;
+    }
+
+    private static void swap(int[] arr, int l, int r) {
+        int tmp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = tmp;
     }
 }
