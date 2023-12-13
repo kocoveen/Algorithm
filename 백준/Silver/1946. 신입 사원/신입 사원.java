872b5c977892a3b32162337200c46250f13abd90
kocoveen <79296295+kocoveen@users.noreply.github.com>
2023-12-13 22:40:03 +0900

    [Silver I] Title: 신입 사원, Time: 892 ms, Memory: 301424 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -12,35 +12,25 @@ public class Main {
 
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            int[][] applicants = new int[n][2];
+            int[] applicants = new int[n+1]; //서류 순위 : 면접 순위
             for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
-                applicants[i][0] = Integer.parseInt(st.nextToken());
-                applicants[i][1] = Integer.parseInt(st.nextToken());
+                int a = Integer.parseInt(st.nextToken()); //서류 순위
+                int b = Integer.parseInt(st.nextToken()); //면접 순위
+                applicants[a] = b;
             }
 
-            Arrays.sort(applicants, (a1, a2) -> {
-                return a1[0] - a2[0];
-            });
-
-            int count = n;
-            int score0 = applicants[0][0];
-            int score1 = applicants[0][1];
-            for (int i = 1; i < n; i++) {
-                //현재 지원자의 (서류, 면접) 순위가 최고 (서류, 면접) 순위보다 낮다면
-                if (score0 <= applicants[i][0] && score1 <= applicants[i][1]) {
-                    count--;
-                } else { //그렇지 않다면, 최고 순위 갱신
-                    score0 = Math.min(score0, applicants[i][0]);
-                    score1 = Math.min(score1, applicants[i][1]);
+            int count = 1; // 최고 서류 순위는 무조건 뽑음
+            int rank = applicants[1]; //최고 면접 순위
+            for (int i = 2; i <= n; i++) {
+                //현재 지원자 면접 순위가 최고 면접 순위보다 높으면 
+                if (applicants[i] < rank) {
+                    count++;
+                    rank = applicants[i]; //최고 면접 순위 갱신
                 }
             }
             sb.append(count).append('\n');
         }
         System.out.print(sb);
-    }
-
-    private static boolean isBetter(int[][] applicants, int i, int j) {
-        return applicants[i][0] > applicants[j][0] && applicants[i][1] > applicants[j][1];
     }
 }
