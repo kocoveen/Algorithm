9a32156e25195966252c384cc2a7e9240cb1e4c9
kocoveen <79296295+kocoveen@users.noreply.github.com>
2023-12-20 16:04:09 +0900

    [Gold V] Title: 트리와 쿼리, Time: 664 ms, Memory: 46568 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+
+    static ArrayList<Integer>[] child;
+    static int[] size;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int n = read(), r = read(), q = read();
+
+        child = new ArrayList[n+1];
+        for (int i = 1; i <= n; i++) {
+            child[i] = new ArrayList<>();
+        }
+        size = new int[n+1];
+
+        for (int i = 0; i < n-1; i++) {
+            int a = read();
+            int b = read();
+            child[a].add(b);
+            child[b].add(a);
+        }
+
+        size[r] = dfs(r, 0);
+
+        while (q-- > 0) {
+            int w = read();
+            sb.append(size[w]).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    private static int dfs(int cur, int root) {
+        size[cur] = 1;
+        for (Integer i : child[cur]) {
+            if (i != root) {
+                size[cur] += dfs(i, cur);
+            }
+        }
+        return size[cur];
+    }
+
+    static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13) System.in.read();
+        return n;
+    }
+}
