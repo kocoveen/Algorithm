4ae39c3ac4cc3084e836f174dc6d669fd43e6d61
kocoveen <79296295+kocoveen@users.noreply.github.com>
2023-12-22 20:58:13 +0900

    [Silver I] Title: 접두사, Time: 136 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/1141. 접두사/접두사.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String[] words = new String[n];
+        for (int i = 0; i < n; i++) {
+            words[i] = br.readLine();
+        }
+        Arrays.sort(words, (w1, w2) -> w2.length() - w1.length());
+
+        HashSet<String> S = new HashSet<>();
+        for (String word : words) {
+            if (S.isEmpty()) {
+                S.add(word);
+                continue;
+            }
+
+            boolean isPrefix = S.stream().anyMatch(inWord -> inWord.startsWith(word));
+            if (!isPrefix) {
+                S.add(word);
+            }
+        }
+        System.out.println(S.size());
+    }
+}
