2fa289c9ba849df5e1e519ff1721862582140f89
kocoveen <79296295+kocoveen@users.noreply.github.com>
2024-01-03 22:04:05 +0900

    [Silver III] Title: 화살표 그리기, Time: 120 ms, Memory: 14080 KB, Score: 21 point -BaekjoonHub

---
+++ 백준/Silver/15975. 화살표 그리기/화살표 그리기.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        List<List<Integer>> dots = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            dots.add(new ArrayList<>());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int x = read();
+            int color = read();
+            dots.get(color).add(x);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(dots.get(i), (i1, i2) -> i1 - i2);
+        }
+
+        int l = 0;
+        for (int i = 1; i <= n; i++) {
+            List<Integer> p = dots.get(i);
+            if (p.isEmpty() || p.size() == 1) {
+                continue;
+            }
+
+            for (int j = 0; j < p.size(); j++) {
+                if (j == 0) {
+                    l += p.get(j + 1) - p.get(j);
+                } else if (j == p.size() - 1) {
+                    l += p.get(j) - p.get(j - 1);
+                } else {
+                    l += Math.min(p.get(j) - p.get(j - 1), p.get(j + 1) - p.get(j));
+                }
+            }
+        }
+        System.out.println(l);
+    }
+
+    public static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+}
