02dc3f9294388ffa84d4a7ce841a8082707fe156
kocoveen <79296295+kocoveen@users.noreply.github.com>
2024-01-07 11:24:13 +0900

    [Silver IV] Title: 가로수, Time: 176 ms, Memory: 15244 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.java
+++ 백준/Silver/2485. 가로수/가로수.java
@@ -1,52 +1,45 @@
-import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer sb = new StringBuffer();
 
-        int N = Integer.parseInt(br.readLine());
-        int[] dot = new int[N];
-        int[] d = new int[N-1];
+    static int n;
+    static int[] map, diff;
 
-        int min = Integer.MAX_VALUE, max = 0;
-        for (int i = 0; i < N; i++) {
-            int x = Integer.parseInt(br.readLine());
-            dot[i] = x;
-            if (min > dot[i])
-                min = dot[i];
-            if (max < dot[i])
-                max = dot[i];
+    public static void main(String[] args) throws Exception {
+        n = read();
+        map = new int[n];
+        for (int i = 0; i < n; i++) {
+            map[i] = read();
         }
 
-        for (int i = 0; i < N - 1; i++)
-            d[i] = Math.abs(dot[i] - dot[i+1]);
+        Arrays.sort(map);
 
-        int minD = d[0];
-        for (int i = 0; i < N - 2; i++) {
-            int g = gcb(d[i], d[i+1]);
-            if (minD > g)
-                minD = g;
+        diff = new int[n];
+        for (int i = 1; i < n; i++) {
+            diff[i] = map[i] - map[i - 1];
         }
 
-        int cnt = ((max - min) / minD) + 1 - dot.length;
-        bw.write(cnt + "");
+        int g = diff[1];
+        for (int i = 1; i < n; i++) {
+            g = gcd(g, diff[i]);
+        }
 
-        bw.flush();
-        br.close();
-        bw.close();
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            sum += (diff[i] / g - 1);
+        }
+        System.out.println(sum);
     }
 
-    private static int gcb(int a, int b) {
-        int A = Math.max(a, b);
-        int B = Math.min(a, b);
-        while (B != 0) {
-            int r = A % B;
-            A = B;
-            B = r;
-        }
-        return A;
+    private static int gcd(int A, int B) {
+        if (B == 0) return A;
+        return gcd(B, A % B);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13) System.in.read();
+        return n;
     }
 }
