a9916b5d7cabdcdde94b190053621f70c82e0e04
kocoveen <79296295+kocoveen@users.noreply.github.com>
2024-01-07 20:08:53 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10496 ms, Memory: 304384 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -8,8 +8,9 @@ public class Main {
     static int n, m, mx;
     static List<Integer>[] graph;
     static boolean[] visit;
-    static int[] count;
+    static Integer[] count;
     static Queue<Integer> q;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         n = read();
@@ -25,7 +26,7 @@ public class Main {
             graph[b].add(a);
         }
 
-        count = new int[n+1];
+        count = new Integer[n+1];
         for (int i = 1; i <= n; i++) {
             count[i] = getTrustCompany(i);
             mx = Math.max(mx, count[i]);
@@ -33,13 +34,18 @@ public class Main {
 
         for (int i = 1; i <= n; i++) {
             if (count[i] == mx) {
-                System.out.print(i + " ");
+                sb.append(i).append(" ");
             }
         }
+        System.out.println(sb);
     }
 
     private static int getTrustCompany(int num) {
-        int count = 0;
+        if (count[num] != null) {
+            return count[num];
+        }
+
+        int cnt = 0;
         visit = new boolean[n+1];
         q = new LinkedList<>();
         q.add(num);
@@ -49,11 +55,13 @@ public class Main {
             for (int i : graph[cur]) {
                 if (visit[i]) continue;
                 q.add(i);
-                count++;
+                cnt++;
                 visit[i] = true;
             }
         }
-        return count;
+
+        count[num] = cnt;
+        return cnt;
     }
 
     private static int read() throws Exception {
@@ -61,15 +69,5 @@ public class Main {
         while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
         if (c == 13) System.in.read();
         return n;
-    }
-
-    private static class Info {
-        int num;
-        int cnt;
-
-        public Info(int num, int cnt) {
-            this.num = num;
-            this.cnt = cnt;
-        }
     }
 }
