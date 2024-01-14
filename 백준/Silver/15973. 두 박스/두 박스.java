d9acbc40ff7e310718011b651ab2ad9802193a89
kocoveen <79296295+kocoveen@users.noreply.github.com>
2024-01-14 11:26:50 +0900

    [Silver I] Title: 두 박스, Time: 124 ms, Memory: 14204 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15973. 두 박스/두 박스.java
+++ 백준/Silver/15973. 두 박스/두 박스.java
@@ -31,7 +31,9 @@ public class Main {
 
     static int read() throws Exception {
         int c, n = System.in.read() & 15;
+        boolean isNegative = n == 13;
+        if (isNegative) n = System.in.read() & 15;
         while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
-        return n;
+        return isNegative ? ~n + 1 : n;
     }
 }
