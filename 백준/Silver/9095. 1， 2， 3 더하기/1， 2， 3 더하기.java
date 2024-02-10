604f836f60a9858bca92803fe76248bd89f9ef82
kocoveen <79296295+kocoveen@users.noreply.github.com>
2024-02-10 19:57:01 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 13948 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,19 +1,20 @@
-import java.io.*;
-
 public class Main {
+    public static void main(String[] args) throws Exception {
+        int t = read();
+        int[] dp = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i <= 11; i++) dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
 
-    static int[] dp = new int[15];
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) sb.append(dp[read()]).append('\n');
+        System.out.print(sb);
+    }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
-        dp[1] = 1; dp[2] = 2; dp[3] = 4;
-        while(T-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            for (int i = 4; i <= n; i++)
-                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-            System.out.println(dp[n]);
-        }
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
