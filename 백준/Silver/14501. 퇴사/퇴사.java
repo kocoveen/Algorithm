74cfe5fa831aabcaaeedf4a43c0093e8d38af266
kocoveen <79296295+kocoveen@users.noreply.github.com>
2024-02-20 22:46:42 +0900

    [Silver III] Title: 퇴사, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,33 +1,29 @@
-import java.io.*;
+import java.io.IOException;
 
 public class Main {
-    static String[] l;
 
-    static int N;
-
-    static int[] T, P;
-    static int[] dp;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        T = new int[N + 2];
-        P = new int[N + 2];
-        dp = new int[N + 2];
-
-        for (int i = 1; i <= N; i++) {
-            l = br.readLine().split(" ");
-            T[i] = Integer.parseInt(l[0]);
-            P[i] = Integer.parseInt(l[1]);
+    static int[] t, p, dp;
+    
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        t = new int[n];
+        p = new int[n];
+        dp = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            t[i] = read(); p[i] = read();
         }
 
-        for (int i = 1; i <= N; i++) {
-            if (i + T[i] <= N + 1) {
-                dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]);
-            }
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + t[i] > n) dp[i] = Math.max(dp[i], dp[i + 1]);
+            else dp[i] = Math.max(dp[i + 1], p[i] + dp[i + t[i]]);
         }
-        System.out.println(dp[N + 1]);
+
+        System.out.println(dp[0]);
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
