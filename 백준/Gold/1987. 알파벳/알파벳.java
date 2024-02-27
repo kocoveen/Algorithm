e692fc514d310cf8fde754db911a1f2f3e89281e
kocoveen <79296295+kocoveen@users.noreply.github.com>
2024-02-27 20:27:25 +0900

    [Gold IV] Title: 알파벳, Time: 852 ms, Memory: 14852 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -5,7 +5,7 @@ public class Main {
 
     static int R, C, mx;
     static char[][] map;
-    static boolean[] visited = new boolean[26];
+    static int visitedBit;
     static int[] dr = {1, 0, -1, 0};
     static int[] dc = {0, 1, 0, -1};
 
@@ -28,22 +28,18 @@ public class Main {
     }
 
     private static void dfs(int r, int c) {
-        visited[map[r][c] - 'A'] = true;
-        mx = Math.max(mx, count(visited));
+        int curVisitedBit = 1 << (map[r][c] - 'A');
+        visitedBit |= curVisitedBit;
+        mx = Math.max(mx, Integer.bitCount(visitedBit));
         for (int i = 0; i < 4; i++) {
             int nr = r + dr[i];
             int nc = c + dc[i];
             if (nr < 0 || R <= nr || nc < 0 || C <= nc) continue;
-            if (visited[map[nr][nc] - 'A']) continue;
-            visited[map[nr][nc] - 'A'] = true;
+            int nxtVisitedBit = 1 << (map[nr][nc] - 'A');
+            if ((visitedBit & nxtVisitedBit) == nxtVisitedBit) continue;
+            visitedBit |= nxtVisitedBit;
             dfs(nr, nc);
-            visited[map[nr][nc] - 'A'] = false;
+            visitedBit &= (~nxtVisitedBit);
         }
-    }
-
-    private static int count(boolean[] visited) {
-        int cnt = 0;
-        for (boolean b : visited) if (b) cnt++;
-        return cnt;
     }
 }
