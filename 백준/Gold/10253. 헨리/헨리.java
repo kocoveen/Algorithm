be216ffc25d90db6efe90ec6016935649fbd06e2
kocoveen <79296295+kocoveen@users.noreply.github.com>
2024-04-02 00:09:17 +0900

    [Gold V] Title: 헨리, Time: 140 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/10253. 헨리/헨리.java
+++ 백준/Gold/10253. 헨리/헨리.java
@@ -3,9 +3,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         int t = read();
         while (t-- > 0) {
-            int num = read(); //분자
-            int denom = read(); //분모
+            int num = read();
+            int denom = read();
+            
             int[] origin = new int[]{num, denom};
+            
             while (!isUnitFraction(origin)) {
                 int[] unitFraction = getUnitFraction(origin);
                 origin = diffFraction(origin, unitFraction);
@@ -17,16 +19,14 @@ public class Main {
     private static int[] getUnitFraction(int[] origin) {
         int num = origin[0];
         int denom = origin[1];
-        while (denom % num != 0) {
-            denom++;
-        }
+        while (denom % num != 0) denom++;
         return new int[]{1, denom / num};
     }
 
     private static int[] diffFraction(int[] origin, int[] unitFraction) {
         int num = origin[0] * unitFraction[1] - origin[1];
         int denom = origin[1] * unitFraction[1];
-        int gcd = gcd(num, denom); //최소공배수
+        int gcd = gcd(num, denom);
         return new int[]{num / gcd, denom / gcd};
     }
 
@@ -42,7 +42,6 @@ public class Main {
     private static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
-        if (c == 13) System.in.read();
         return n;
     }
 }
