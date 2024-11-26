dbe81285dac29033a22f23386ef770fa0d44bf8a
장웅기 <79296295+kocoveen@users.noreply.github.com>
2024-11-26 15:51:12 +0900

    [Gold IV] Title: 단어 수학, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 
@@ -8,30 +7,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        String[] arr = new String[n];
-        int[] alpha = new int[26];
+        String[] words = new String[n];
+        int[] nums = new int[26];
 
-        for (int i = 0; i < n; i++){
-            arr[i] = br.readLine();
-        }
+        for (int i = 0; i < n; i++) words[i] = br.readLine();
 
-        for (String string : arr) {
-            int tmp = (int) Math.pow(10, string.length()-1);
-            for (int i = 0; i < string.length(); i++) {
-                alpha[string.charAt(i) - 'A'] += tmp;
-                tmp /= 10;
+        for (String word : words) {
+            int digit = (int) Math.pow(10, word.length() - 1);
+            for (int i = 0; i < word.length(); i++) {
+                nums[word.charAt(i) - 'A'] += digit;
+                digit /= 10;
             }
         }
 
-        Arrays.sort(alpha);
+        Arrays.sort(nums);
         int sum = 0;
         int num = 9;
-        for (int i = alpha.length-1; i >= 0; i--) {
-            if (alpha[i] == 0) {
-                break;
-            }
-            sum += num * alpha[i];
-            num--;
+        for (int i = nums.length-1; i >= 0; i--) {
+            if (nums[i] == 0) break;
+            sum += num-- * nums[i];
         }
         System.out.println(sum);
     }
