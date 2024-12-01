cbe1d655bf20ac82268146d2914154f8c0596e81
장웅기 <79296295+kocoveen@users.noreply.github.com>
2024-12-01 23:51:09 +0900

    [Gold III] Title: 여왕벌, Time: 2596 ms, Memory: 72912 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.java
+++ 백준/Gold/10836. 여왕벌/여왕벌.java
@@ -1,74 +1,30 @@
 public class Main {
 
     static int M, N;
-    static int[][][] tmp;
-    static int[] growArray;
+    static int[] tmp;
 
     public static void main(String[] args) throws Exception {
         M = read(); N = read();
-        tmp = new int[N][M][M];
-        growArray = new int[3];
+        tmp = new int[2 * M - 1];
 
-        for (int i = 0; i < N; i++) {
-            fill(i);
-        }
+        for (int day = 0; day < N; day++) {
+            int zero = read();
+            int one = read();
+            int two = read();
 
-        print();
-    }
-
-    static void fill(int n) throws Exception {
-        growArray[0] = read();
-        growArray[1] = read();
-        growArray[2] = read();
-
-        for (int i = M-1; i >= 0; i--) { tmp[n][i][0] = getGrow(); }
-        for (int i = 1; i < M; i++) { tmp[n][0][i] = getGrow(); }
-
-        for (int i = 1; i < M; i++) {
-            for (int j = 1; j < M; j++) {
-                tmp[n][i][j] = maxGrow(n, i, j);
+            for (int i = zero; i < zero + one; i++) {
+                tmp[i] += 1;
             }
-        }
-    }
 
-    private static int getGrow() {
-        if (growArray[0] != 0) {
-            growArray[0]--;
-            return 0;
-        }
-        if (growArray[1] != 0) {
-            growArray[1]--;
-            return 1;
-        }
-        return 2;
-    }
-
-    static int maxGrow(int n, int r, int c) {
-        return max(tmp[n][r-1][c], tmp[n][r][c-1], tmp[n][r-1][c-1]);
-    }
-
-    static int max(int... values) {
-        int max = 0;
-        for (int i : values) {
-            max = Math.max(max, i);
-        }
-        return max;
-    }
-
-    static void print() {
-        int[][] printArray = new int[M][M];
-
-        for (int k = 0; k < N; k++) {
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j < M; j++) {
-                    printArray[i][j] += tmp[k][i][j];
-                }
+            for (int i = zero + one; i < tmp.length; i++) {
+                tmp[i] += 2;
             }
         }
 
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < M; j++) {
-                System.out.print(printArray[i][j] + 1 + " ");
+                if (j == 0) { System.out.print(tmp[M - 1 - i] + 1 + " "); }
+                else { System.out.print(tmp[M + j - 1] + 1 + " "); }
             }
             System.out.println();
         }
@@ -76,9 +32,7 @@ public class Main {
 
     static int read() throws Exception {
         int c, n = System.in.read() & 15;
-        boolean isNegative = n == 13;
-        if (isNegative) n = System.in.read() & 15;
         while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
-        return isNegative ? ~n + 1 : n;
+        return n;
     }
 }
