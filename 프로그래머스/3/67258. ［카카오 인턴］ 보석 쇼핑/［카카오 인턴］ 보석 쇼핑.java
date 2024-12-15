fd6e49673155866d295ed64f806523b6b7820262
장웅기 <79296295+kocoveen@users.noreply.github.com>
2024-12-15 19:42:32 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 95.85 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,59 +1,55 @@
 import java.util.*;
 
 class Solution {
-    Set<String> set = new HashSet<>();
-    Map<String, Integer> map = new HashMap<>();   
-    
-    int mn = Integer.MAX_VALUE;
-    
+
+    static Set<String> gemSet;
+    static Map<String, Integer> gemCountMap;
+
+    static int mnVal = Integer.MAX_VALUE;
+    static int mnL, mnR;
+
     public int[] solution(String[] gems) {
-        int[] answer = new int[2];
-        
-        for (String gem : gems) {
-            set.add(gem);
-        }
-        
-        int[] end = new int[gems.length];
-        
+        gemSet = new HashSet<>();
+        gemCountMap = new HashMap<>();
+
+        gemSet.addAll(Arrays.asList(gems));
+
         int l = 0, r = 0;
-        while (l <= r) {
-            if (map.size() != set.size()) {
-                if (r == gems.length) {
-                    break;
+        while (l <= r && l < gems.length) {
+            if (r == gems.length) {
+                if (gemCountMap.size() == gemSet.size() && mnVal > (r - l)) {
+                    mnL = l;
+                    mnR = r;
+                    mnVal = mnR - mnL;
                 }
-                map.put(gems[r], map.getOrDefault(gems[r], 0) + 1);
-                r++;
-            } else {
-                if (l == gems.length) {
-                    break;
-                }
-                end[l] = r;
-                map.put(gems[l], map.getOrDefault(gems[l], 0) - 1);
-                if (map.get(gems[l]) == 0) {
-                    map.remove(gems[l]);
+
+                gemCountMap.put(gems[l], gemCountMap.get(gems[l]) - 1);
+                if (gemCountMap.get(gems[l]) == 0) {
+                    gemCountMap.remove(gems[l]);
                 }
                 l++;
+            } else {
+                if (gemCountMap.size() != gemSet.size()) {
+                    gemCountMap.put(gems[r], gemCountMap.getOrDefault(gems[r], 0) + 1);
+                    r++;
+
+                } else {
+                    if (gemCountMap.size() == gemSet.size() && mnVal > (r - l)) {
+                        mnL = l;
+                        mnR = r;
+                        mnVal = mnR - mnL;
+                    }
+
+                    gemCountMap.put(gems[l], gemCountMap.get(gems[l]) - 1);
+                    if (gemCountMap.get(gems[l]) == 0) {
+                        gemCountMap.remove(gems[l]);
+                    }
+
+                    l++;
+                }
             }
         }
-        
-        if (r == gems.length && map.size() == set.size()) {
-            end[l] = r;
-        }
-        
-        for (int i = 0; i < end.length; i++) {
-            if (end[i] == 0) continue;
-            if (mn > end[i] - i) {
-                mn = end[i] - i;
-                answer[0] = i + 1;
-                answer[1] = end[i];
-            }
-        }
-        
-        // for(int e : end) {
-        //     System.out.print(e + " ");   
-        // }
-        
-        
-        return answer;
+        System.out.println(mnL + " " + mnR);
+        return new int[]{mnL+1, mnR};
     }
 }
