b8aeb22b9af8f06337a14f99f49590f8ab7fcb6e
장웅기 <79296295+kocoveen@users.noreply.github.com>
2025-12-29 18:56:35 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -2,40 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
-
-        Loop1 :
-        for (int j = 0; j < T; j++) {
-
-            String s = br.readLine();
-
-            Stack<Character> S = new Stack<>();
-            for (int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
-                if (c == '(')
-                    S.push(c);
-                else if (c == ')') {
-                    if (S.isEmpty()) {
-                        sb.append("NO").append("\n");
-                        continue Loop1;
-                    }
-
-                    if (S.peek() == '(')
-                        S.pop();
-                }
-            }
-            if (S.isEmpty())
-                sb.append("YES").append("\n");
-            else
-                sb.append("NO").append("\n");
+  public static void main(String[] args) throws Exception {
+    var reader = new BufferedReader(new InputStreamReader(System.in));
+        
+    int T = Integer.parseInt(reader.readLine());
+    
+    while (T-- > 0) {
+      Deque<Character> deque = new ArrayDeque<>();
+      String string = reader.readLine();
+      boolean isVPS = true;
+      for (char c : string.toCharArray()) {
+        if (c == '(') {
+          deque.push(c);
+        } else if (c == ')') {
+          if (deque.peek() == null) {
+            isVPS = false;
+            break;
+          } else {
+            deque.pop();
+          }
         }
-
-        System.out.print(sb);
+      }
+          
+      if (!deque.isEmpty()) {
+        isVPS = false;
+      }
+          
+      if (isVPS) {
+          System.out.println("YES");
+      } else {
+          System.out.println("NO");
+      }
     }
+  }
 }
