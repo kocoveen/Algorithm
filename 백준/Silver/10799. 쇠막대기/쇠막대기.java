09e47b182a90e190d650446dc5f2234d7c9bb51f
장웅기 <79296295+kocoveen@users.noreply.github.com>
2025-12-31 17:56:37 +0900

    [Silver II] Title: 쇠막대기, Time: 144 ms, Memory: 16612 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,32 +1,29 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
+  public static void main(String[] args) throws Exception {
+    var reader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String VPS = br.readLine();
-
-        Stack<Character> S = new Stack<>();
-
-        int sum = 0;
-        for (int i = 0; i < VPS.length(); i++) {
-            if (VPS.charAt(i) == '(')
-                S.push('(');
-            else {
-                if (VPS.charAt(i - 1) == '(') {
-                    S.pop();
-                    sum += S.size();
-                } else {
-                    S.pop();
-                    sum += 1;
-                }
-            }
+    Deque<Character> stack = new ArrayDeque<>();
+    
+    String input = reader.readLine();
+    
+    int count = 0;
+    char prev = '=';
+    for (char c : input.toCharArray()) {
+      if (c == '(') {
+        stack.addLast(c);
+      } else {
+        stack.removeLast();
+        if (prev == '(') {
+          count += stack.size();
+        } else {
+          count++;
         }
-
-        System.out.println(sum);
+      }
+      prev = c;
     }
+    System.out.println(count);
+  }
 }
