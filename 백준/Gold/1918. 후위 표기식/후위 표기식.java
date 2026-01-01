4e3b5b223d32487f84e540d7432b8f6ad9691d6b
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-02 00:46:24 +0900

    [Gold II] Title: 후위 표기식, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.java
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -1,84 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        // 초기화
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String expr = br.readLine();
-        System.out.println(convert(expr));
+  
+  public static void main(String[] args) throws Exception {
+    var reader = new BufferedReader(new InputStreamReader(System.in));
+    var writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    char[] infix = reader.readLine().toCharArray(); // 중위표기식
+    
+    Deque<Character> stack = new ArrayDeque<>(); // 스택
+    
+    for (char c : infix) {
+      // 피연산자이면 그대로 출력
+      if ('A' <= c && c <='Z') {
+        writer.write(c);
+        continue;
+      }
+      
+      // 연산자 or 괄호일 때, 비었으면 push
+      if (stack.isEmpty()) {
+        stack.addLast(c);
+        continue;
+      }
+      
+      // 연산자가 스택의 top보다 우선순위가 높으면 push
+      // 연산자가 스택의 top보다 우선순위가 낮거나 같다면 pop하여 출력, 현재 연산자는 push
+      // 여는 괄호 => push
+      // 닫는 괄호 => 여는 괄호가 나올 때까지 pop, 여는 괄호는 버림.
+      switch (c) {
+        case '+': 
+        case '-': {
+          while (!stack.isEmpty() && stack.getLast() != '(') {
+            char pop = stack.removeLast();
+            writer.write(pop);
+          }
+          stack.addLast(c); 
+          break;
+        }
+        
+        case '*': 
+        case '/': {
+          while (!stack.isEmpty() && (stack.getLast() == '*' || stack.getLast() == '/')) {
+            char pop = stack.removeLast();
+            writer.write(pop);
+          }
+          stack.addLast(c);
+          break;
+        }
+        
+        case '(': {
+          stack.addLast(c);
+          break;
+        }
+        
+        case ')': {
+          while (stack.getLast() != '(') {
+            char pop = stack.removeLast();
+            writer.write(pop);
+          }
+          stack.removeLast();
+          break;
+        }
+      }
     }
 
-    static String convert(String exp) { // 중위표기식을 후위표기식으로 변경하는 함수
-        StringBuilder sb = new StringBuilder(); // 후위표기식
-        Stack<Character> s = new Stack<>(); // (피연산자가 아닌) 기호를 넣을 식
-        char c, t; // c는 수식에서 읽을 문자, t는 스택에서 pop된 문자
-        int n = exp.length(); // 수식문 길이
-
-        for (int i = 0; i < n; i++) {
-            c = exp.charAt(i);
-            switch (c) {
-                /* 
-                if (연산자) {
-                    while (스택이 비지 않음 && c의 우선 순위 >= 스택 맨 위 항목의 우선순위) {
-                        후위표기식에 pop한 것 붙임, 즉 c보다 작거나 같은 우선순위의 항목을 sb에 추가
-                    }
-                    마지막으로 c를 스택에 넣음
-                }
-                */
-                case '+': case '-': case '*': case '/':
-                    while (!s.isEmpty() && prec(c) >= prec(s.peek())) {
-                        sb.append(s.pop());
-                    }
-                    s.push(c);
-                    break;
-
-                case '(':
-                    s.push(c);
-                    break;
-
-                case ')':
-                /* 
-                t = 스택에서 꺼낸 값
-                while (t가 '(' 아니라면) {
-                    후위표기식에 t 붙임
-                    t = 스택에서 꺼낸 값
-                }
-                */
-                    t = s.pop();
-                    while (t != '(') {
-                        sb.append(t);
-                        t = s.pop();
-                    }
-                    break;
-
-                default:
-                    sb.append(c);
-                    break;
-            }
-        }
-
-        /*
-        스택이 빌 때까지 sb에 붙임
-         */
-        while(!s.isEmpty()) {
-            sb.append(s.pop());
-        }
-        return sb.toString();
+    // 더 이상 스캔할 입력 리스트 값이 없다면, 하나씩 pop하여 출력
+    while (!stack.isEmpty()) {
+      char pop = stack.removeLast();
+      writer.write(pop);        
     }
-
-    static int prec(char op) { // 연산자 우선순위를 리턴하는 함수
-        switch (op) {
-            case '(': case ')':
-                return 2;
-            case '+': case '-':
-                return 1;
-            case '*': case '/':
-                return 0;
-        }
-        return 3;
-    }
+    writer.flush();
+  }
 }
