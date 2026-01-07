63f27e5ab42914c14d3e6f1a20518442d69cf5fa
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-07 14:00:30 +0900

    [Gold V] Title: 트리, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,47 +1,62 @@
-import java.io.IOException;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 public class Main {
-    private static Set<Integer>[] tree;
-    private static int[] parents;
-    private static int N, D, R, C;
+    
+    static List<List<Integer>> graph;
+    static int deleted;
 
-    public static void main(String[] args) throws IOException {
-        N = read();
-        parents = new int[N];
-        for (int i = 0; i < N; i++) parents[i] = read();
-        D = read();
-
-        tree = new HashSet[N];
-        for (int i = 0; i < N; i++) tree[i] = new HashSet<>();
+    public static void main(String[] args) throws Exception {
+        int N = read();
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        int root = 0;
         for (int i = 0; i < N; i++) {
-            if (parents[i] < 0) R = i;
-            else tree[parents[i]].add(i);
+            int node = read();
+            if (node == -1) {
+                root = i;
+            } else {
+                graph.get(node).add(i);
+            }
+        }
+        deleted = read();
+        li: 
+        for (List<Integer> list : graph) {
+            for (int elem : list) {
+                if (elem == deleted) {
+                    list.remove(Integer.valueOf(deleted));
+                    break li;
+                } 
+            }
         }
 
-        tree[D].clear();
-        if (parents[D] >= 0) tree[parents[D]].remove(D);
-
-        // BFS로 찾기
-        Deque<Integer> deque = new ArrayDeque<>();
-        deque.add(R);
-        while (!deque.isEmpty()) {
-            int i = deque.poll();
-            if (i != D && tree[i].isEmpty()) C++;
-            deque.addAll(tree[i]);
-        }
-        System.out.println(C);
+        System.out.println(traversal(root));
     }
 
-    private static int read() throws IOException {
+    static int traversal(int node) {
+        if (node == deleted) { 
+            return 0;
+        }
+
+        if (graph.get(node).isEmpty()) {
+            return 1;
+        }
+
+        int sum = 0;
+        for (int elem : graph.get(node)) {
+            sum += traversal(elem);
+        }
+        return sum;
+    }
+
+    private static int read() throws Exception {
         int c, n = System.in.read() & 15;
         boolean isNegative = n == 13;
         if (isNegative) n = System.in.read() & 15;
         while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
-        return isNegative ? ~n + 1 : n;
+        return isNegative ? ~n+1 : n;
     }
 }
