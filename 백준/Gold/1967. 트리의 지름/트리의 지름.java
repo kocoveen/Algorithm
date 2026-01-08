e589e559183e8382c27f3598be1a4547e9ddcb90
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-08 17:35:09 +0900

    [Gold IV] Title: 트리의 지름, Time: 148 ms, Memory: 18744 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,60 +1,62 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    static List<List<Node>> graph = new ArrayList<>();
-    static boolean[] visited;
-    static int maxIndex = 1;
-    static int maxCost = 0;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < n - 1; i++) {
-            int parent = sc.nextInt();
-            int child = sc.nextInt();
-            int cost = sc.nextInt();
-
-            graph.get(parent).add(new Node(child, cost));
-            graph.get(child).add(new Node(parent, cost));
-        }
-
-        visited = new boolean[n + 1];
-        visited[1] = true;
-        dfs(1, 0);
-
-        visited = new boolean[n + 1];
-        visited[maxIndex] = true;
-        dfs(maxIndex, 0);
-        System.out.println(maxCost);
-    }
-
-    private static void dfs(int start, int cost) {
-        if (maxCost < cost) {
-            maxCost = cost;
-            maxIndex = start;
-        }
-
-        for (Node node : graph.get(start)) {
-            if (!visited[node.num]) {
-                visited[node.num] = true;
-                dfs(node.num, cost + node.cost);
-            }
-        }
-    }
-
     static class Node {
-        int num;
-        int cost;
-
+        int num, cost;
         public Node(int num, int cost) {
             this.num = num;
             this.cost = cost;
         }
+    }
+
+    static Set<Node>[] graph;
+    static boolean[] visited;
+    static int maxCost, maxNum;
+
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        graph = new HashSet[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new HashSet<>();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int u = read();
+            int v = read();
+            int cost = read();
+
+            graph[u].add(new Node(v, cost));
+            graph[v].add(new Node(u, cost));
+        }
+
+        visited = new boolean[n + 1];
+        visited[1] = true;
+        traversal(1, 0);
+        
+        visited = new boolean[n + 1];
+        visited[maxNum] = true;
+        traversal(maxNum, 0);
+        System.out.println(maxCost);
+    }
+
+    private static void traversal(int num, int cost) {
+        if (maxCost < cost) {
+            maxCost = cost;
+            maxNum = num;
+        }
+
+        for (Node n : graph[num]) {
+            if (visited[n.num]) continue;
+            visited[n.num] = true;
+            traversal(n.num, cost + n.cost);
+            visited[n.num] = false;
+        }
+    } 
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
