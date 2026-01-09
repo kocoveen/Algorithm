34747046b09758da6d222def3ebcb8e33aa12e3d
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-09 18:07:02 +0900

    [Gold I] Title: 트리의 순회, Time: 228 ms, Memory: 28868 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -1,62 +1,53 @@
+
 public class Main {
 
-    static int n, idx = 1;
-    static int[] preTree;
-    static int[] postTree;
-    static int[] inTree;
-    static int[] indexes; // inTree 값의 인덱스 저장
+    static int[] postorders, inorders, indexes, preorders;
+    static int N, index = 0;
 
     public static void main(String[] args) throws Exception {
-        n = read();
-        preTree = new int[n + 1];
-        inTree = new int[n + 1];
-        postTree = new int[n + 1];
+        N = read();
+        
+        postorders = new int[N];
+        inorders = new int[N];
+        indexes = new int[N];
+        preorders = new int[N];
 
-        indexes = new int[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            inTree[i] = read();
-            indexes[inTree[i]] = i;
+        for (int i = 0; i < N; i++) {
+            inorders[i] = read();
+            indexes[inorders[i]-1] = i;
+        }
+        
+        for (int i = 0; i < N; i++) {
+            postorders[i] = read();
         }
 
-        for (int i = 1; i <= n; i++) postTree[i] = read();
-
-        dfs(1, n + 1, 1, n + 1);
-
+        buildPreOrder(0, N-1, 0, N-1);
+        
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= n; i++) {
-            sb.append(preTree[i]).append(" ");
+        for (int i = 0; i < N; i++) {
+            sb.append(preorders[i]).append(" ");
         }
         System.out.println(sb);
     }
 
-    private static void dfs(int in_st, int in_en, int post_st, int post_en) {
+    private static void buildPreOrder(int postStart, int postEnd, int inStart, int inEnd) {
+        if (postStart > postEnd || inStart > inEnd) {
+            return;
+        }
 
-        if (in_st >= in_en || post_st >= post_en) return;
+        // 1. 후위 순회 배열로 부모 찾기 (루트는 무조건 마지막)
+        int parentValue = postorders[postEnd];
 
-        int root = postTree[post_en - 1];
+        // 2. 중위 순회 배열에서 부모를 찾고 양 자식의 길이 구하기
+        int parentIndexInInorder = indexes[parentValue-1];
 
-        preTree[idx++] = root;
-
-        int in_root_index = indexes[root];
-
-        int l_size = in_root_index - in_st; // 좌측 자식 트리 size
-        int r_size = in_en - in_root_index - 1; // 우측 자식 트리 size
-
-        int l_in_st = in_st;
-        int l_in_en = l_in_st + l_size;
-        int l_post_st = post_st;
-        int l_post_en = post_st + l_size;
-
-        int r_in_st = in_root_index + 1;
-        int r_in_en = r_in_st + r_size;
-        int r_post_st = post_st + l_size;
-        int r_post_en = r_post_st + r_size;
-
-        dfs(l_in_st, l_in_en, l_post_st, l_post_en);
-        dfs(r_in_st, r_in_en, r_post_st, r_post_en);
+        // 3. 오른쪽 트리의 길이를 구함
+        int leftSize = parentIndexInInorder - inStart;
+        // 4. 양 트리의 후위 중위 위치를 넘겨 재귀
+        preorders[index++] = parentValue;
+        buildPreOrder(postStart, postStart + leftSize - 1, inStart, parentIndexInInorder - 1);
+        buildPreOrder(postStart + leftSize, postEnd - 1, parentIndexInInorder + 1, inEnd);
     }
-
 
     public static int read() throws Exception {
         int c, n = System.in.read() & 15;
