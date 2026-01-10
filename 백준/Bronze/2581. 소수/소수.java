6e2187d1b4b8e130fe0fe0c2ff2c2029a461e394
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-10 16:04:12 +0900

    [Bronze II] Title: 소수, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,28 +1,37 @@
-import java.util.*;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        int N = read();
+        int M = read();
 
-        int sum = 0; int min = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+
         Loop1:
         for (int i = N; i <= M; i++) {
             if (i == 1) continue;
 
+
             for (int j = 2; j <= Math.sqrt(i); j++) {
-                if (i % j == 0)
+                if (i % j == 0) {
                     continue Loop1;
+                }
             }
-            if (sum == 0) min = i;
             sum += i;
+            if (min == Integer.MAX_VALUE) {
+                min = i;
+            }
         }
-        if (sum != 0) {
-            System.out.println(sum);
-            System.out.println(min);
-            return;
+
+        if (sum > 0) {
+            System.out.print("%d\n%d".formatted(sum, min));
+        } else {
+            System.out.print(-1);
         }
-        System.out.println(-1);
+    }
+
+    public static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
