09b8a8ae59e3b5d9a214978393daebff0d3029bf
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-11 13:53:23 +0900

    [Bronze II] Title: 진법 변환, Time: 100 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,32 +1,14 @@
-import java.io.*;
-import java.util.*;
-import java.lang.Math;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        var reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] line =  reader.readLine().split(" ");
 
-        String s = st.nextToken();
-        int base = Integer.parseInt(st.nextToken());
+        String num = line[0];
+        int radix = Integer.parseInt(line[1]);
 
-        int pow = 1;
-        int sum = 0;
-        for (int i = s.length()-1; i >= 0; i--) {
-            char c = s.charAt(i);
-            if ('A' <= c && c <= 'Z') {
-                sum += (c - 'A' + 10) * pow;
-            } else {
-                sum += (c - '0') * pow;
-            }
-            pow *= base;
-        }
-        if (sum <= 1000000000)
-            bw.write("" + sum);
-
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.println(Integer.valueOf(num, radix));
     }
 }
