6687c9e2300491d31f261a6d92e01437a49cfc4d
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-12 14:05:31 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 164 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -1,37 +1,26 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    static String[] l;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
 
-    static int N, K;
-    static int cnt, val;
-
-    static boolean[] E;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        l = br.readLine().split(" ");
-        N = Integer.parseInt(l[0]);
-        K = Integer.parseInt(l[1]);
-
-        E = new boolean[N + 1];
-
+        boolean[] sieve = new boolean[N + 1];
+        
+        int count = 0;
         for (int i = 2; i <= N; i++) {
-            if (E[i]) continue;
+            if (sieve[i]) continue;
             for (int j = i; j <= N; j += i) {
-                if (!E[j]) {
-                    cnt++;
-                    E[j] = true;
-                    val = j;
-                    if (cnt == K) {
-                        System.out.println(val);
+                if (!sieve[j]) {
+                    sieve[j] = true;
+                    count++;
+                    if (count == K) {   
+                        System.out.println(j);
                         System.exit(0);
                     }
                 }
             }
         }
-
-
     }
 }
