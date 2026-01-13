7dba4a0a49e20ce3826fd6c7bab0dae54ea29cf3
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-13 14:32:58 +0900

    [Silver II] Title: 폰 호석만, Time: 192 ms, Memory: 18976 KB, Score: 24 point -BaekjoonHub

--- 백준/Silver/21275. 폰 호석만/폰 호석만.java
+++ 백준/Silver/21275. 폰 호석만/폰 호석만.java
@@ -1,5 +1,4 @@
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -17,15 +16,13 @@ public class Main {
         int count = 0;
         for (int i = 2; i <= 36; i++) {
             try {
-                BigInteger n = new BigInteger(a, i);
-                if (n.bitCount() >= 63) continue;
+                long n = Long.parseLong(a, i);
                 for (int j = 2; j <= 36; j++) {
                     if (i == j) continue;
                     try {
-                        BigInteger m = new BigInteger(b, j);
-                        if (m.bitCount() >= 63) continue;
+                        long m = Long.parseLong(b, j);
                         if (Objects.equals(n, m)) {
-                            list.add(new long[]{n.longValue(), i, j});
+                            list.add(new long[]{n, i, j});
                             count++;
                         }
                     } catch (NumberFormatException e) { }
