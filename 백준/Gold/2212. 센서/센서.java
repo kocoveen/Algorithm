73422c0fcfd056f254ea1e6889ca7a2910bdf64f
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-18 16:42:49 +0900

    [Gold V] Title: 센서, Time: 364 ms, Memory: 30996 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        int[] arr = new int[N];
+        int[] diff = new int[N-1];
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N-1; i++) {
+            diff[i] = arr[i+1] - arr[i];
+        }
+
+        Arrays.sort(diff);
+
+        int sum = 0;
+        for (int i = 0; i < N-K; i++) {
+            sum += diff[i];
+        }
+
+        System.out.println(sum);
+    }
+}
