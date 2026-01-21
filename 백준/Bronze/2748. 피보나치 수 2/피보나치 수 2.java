4adba96f1c0f81338767bf0c421f8e3b21302599
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-21 12:19:19 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 172 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,21 +1,18 @@
-import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static long[] dp;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = scanner.nextInt();
 
-        N = Integer.parseInt(br.readLine());
-        dp = new long[N + 1];
-        dp[0] = 0;
-        dp[1] = 1;
+        long[] arr = new long[N+2];
+        arr[1] = 1L;
 
-        for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        for (int i = 0; i < N; i++) {
+            arr[i+2] = arr[i+1] + arr[i];
         }
 
-        System.out.println(dp[N]);
+        System.out.println(arr[N]);
     }
 }
