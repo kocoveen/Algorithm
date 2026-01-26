91f372d3811e4617cb87072b4c0367768b3b0048
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-26 12:43:04 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 168 ms, Memory: 16012 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,36 +1,46 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static String[] l;
-
-    static int N, max = Integer.MIN_VALUE;
-
-    static int[] arr;
+    static StringTokenizer st;
+    static int[] Num;
     static Integer[] dp;
+    static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
 
-        arr = new int[N + 1];
-        dp = new Integer[N + 1];
-        l = br.readLine().split(" ");
+        Num = new int[N+1];
+        dp = new Integer[N+1];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++)
-            arr[i] = Integer.parseInt(l[i - 1]);
+            Num[i] = Integer.parseInt(st.nextToken());
 
-        dp[0] = arr[0];
-        for (int i = 1; i <= N; i++) {
-            int tmp = 0;
-            for (int j = 0; j < i; j++)
-                if (arr[i] > arr[j])
-                    tmp = Math.max(dp[j], tmp);
-            dp[i] = tmp + 1;
-        }
+        dp[0] = 0;
 
+        for (int i = 1; i <= N; i++)
+            LIS(i);
+
+        int max = dp[0];
         for (int i = 1; i <= N; i++)
             max = Math.max(max, dp[i]);
 
-        System.out.println(max);
+        bw.write(max + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void LIS(int index) {
+        int max = dp[0];
+        for (int i = 0; i < index; i++)
+            if (Num[index] > Num[i] && max < dp[i])
+                max = dp[i];
+        dp[index] = max + 1;
     }
 }
