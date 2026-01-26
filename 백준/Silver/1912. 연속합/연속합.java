bb7f80070578824c6906554e2578ad02716919a7
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-26 13:31:06 +0900

    [Silver II] Title: 연속합, Time: 272 ms, Memory: 24208 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,32 +1,45 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static String[] l;
+    static StringBuilder sb = new StringBuilder();
+    static int[] Num;
+    static int[] Sum;
+    static int N;
+    static int max;
 
-    static int N, max = Integer.MIN_VALUE;
-
-    static int[] arr;
-    static Integer[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
 
-        arr = new int[N + 1];
-        dp = new Integer[N + 1];
-        l = br.readLine().split(" ");
-        for (int i = 0; i < N; i++)
-            arr[i] = Integer.parseInt(l[i]);
+        Num = new int[100001];
+        Sum = new int[100001];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        dp[0] = arr[0];
-        for (int i = 1; i < N; i++)
-            dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
-
+        Arrays.fill(Sum, -1001);
         for (int i = 0; i < N; i++) {
-            max = Math.max(dp[i], max);
+            int n = Integer.parseInt(st.nextToken());
+            Num[i] = n;
         }
+        bw.write(findMax(N) + "\n");
 
-        System.out.println(max);
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static int findMax(int N) {
+        Sum[0] = Num[0];
+        max = Num[0];
+        for (int i = 1; i < N; i++) {
+            Sum[i] = Math.max(Sum[i - 1] + Num[i], Num[i]);
+            if (max < Sum[i])
+                max = Sum[i];
+        }
+        return max;
     }
 }
