26c0a1af27e6308ea759bec7c615426eca517f48
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-28 13:18:57 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 240 ms, Memory: 21184 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -2,28 +2,38 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int n;
+    static int[] A;
+    static int[] dp;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[] A = new int[n];
-        int[] dp = new int[n];
+        n = sc.nextInt();
+        A = new int[n];
+        dp = new int[n];
 
         for (int i = 0; i < n; i++) {
             A[i] = sc.nextInt();
+            dp[i] = -1;
         }
 
         int result = 0;
-
         for (int i = 0; i < n; i++) {
-            dp[i] = A[i];
-            for (int j = 0; j < i; j++) {
-                if (A[j] < A[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + A[i]);
-                }
-            }
-            result = Math.max(result, dp[i]);
+            result = Math.max(result, dp(i));
         }
         System.out.println(result);
+    }
+
+    private static int dp(int i) {
+        if (dp[i] != -1) return dp[i];
+
+        int maxSum = A[i];
+        for (int j = 0; j < i; j++) {
+            if (A[j] < A[i]) {
+                maxSum = Math.max(maxSum, dp(j) + A[i]);
+            }
+        }
+        return dp[i] = maxSum;
     }
 }
