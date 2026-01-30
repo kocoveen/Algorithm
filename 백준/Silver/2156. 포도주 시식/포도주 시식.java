bafb5427c312aac2f45bc8bc986c0aee4d6c8ad6
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-01-30 13:52:37 +0900

    [Silver I] Title: 포도주 시식, Time: 148 ms, Memory: 16148 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,31 +1,44 @@
-import java.io.*;
-
 public class Main {
 
-    static int N, max;
-    static int[] arr;
+    static int[] arr; // 포도주가 있는 위치
     static Integer[] dp;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = read();
+        }
 
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N + 1];
-        dp = new Integer[N + 1];
-
-        for (int i = 1; i <= N; i++)
-            arr[i] = Integer.parseInt(br.readLine());
-
-        System.out.println(func(N));
+        dp = new Integer[n + 1];
+        System.out.println(func(n));
     }
 
-    private static int func(int n) {
-        if (n <= 0) return 0;
-        if (n == 1) return arr[1];
+    // dp[i]를 구하는 메서드 (3연속 먹지 못함)
+    public static int func(int i) {
+        if (i <= 0) return 0; // 경계조건 1
+        if (i == 1) return arr[1]; // 경계조건 2
 
-        if (dp[n] == null)
-            dp[n] = Math.max(func(n - 1), Math.max(func(n - 2), arr[n - 1] + func(n - 3)) + arr[n]);
-        
-        return dp[n];
+        if (dp[i] == null) {
+            dp[i] = max(
+                    func(i - 2) + arr[i], // 포도주[i]를 먹을 수 있고, 한 칸을 건너 뛴 경우 
+                    func(i - 3) + arr[i - 1] + arr[i], // 포도주[i]를 먹고, 인접한 포도주[i-1]를 먹어 포도주[i-3]을 먹는 경우
+                    func(i - 1) // 포도주[i]를 못 먹는 경우
+            );
+        }
+        return dp[i];
+    }
+
+    // 최대를 구하는 메서드
+    private static int max(int... nums) {
+        int max = 0;
+        for (int num : nums) max = Math.max(max, num);
+        return max;
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
