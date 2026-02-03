e187960df5cf45a7316133cf9f6cd36c3221ec3b
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-02-03 11:35:01 +0900

    [Silver I] Title: 주지수, Time: 840 ms, Memory: 41492 KB -BaekjoonHub

--- 백준/Silver/15724. 주지수/주지수.java
+++ 백준/Silver/15724. 주지수/주지수.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        int n = read();
+        int m = read();
 
         int[][] map = new int[n+1][m+1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                map[i][j] = sc.nextInt();
+                map[i][j] = read();
             }
         }
 
@@ -21,14 +21,20 @@ public class Main {
             }
         }
 
-        int k = sc.nextInt();
+        int k = read();
         while (k-- > 0) {
-            int x1 = sc.nextInt();
-            int y1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            int y2 = sc.nextInt();
+            int x1 = read();
+            int y1 = read();
+            int x2 = read();
+            int y2 = read();
         
             System.out.println(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]);
         }
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
