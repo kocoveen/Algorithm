3326e7efc4093b549c2f8786b1793f9965172a6e
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-02-07 13:12:28 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2056 ms, Memory: 360380 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,40 +1,44 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static String[] line;
 
-    static int N, M;
+    static int n, m;
     static int[] arr;
-    static int[][] dp;
+    static Integer[][] dp;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
 
-        N = Integer.parseInt(br.readLine());
-        line = br.readLine().split(" ");
-        arr = new int[N + 1];
-        dp = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(line[i - 1]);
+        n = sc.nextInt();
 
-        // length 1
-        for (int i = 1; i <= N; i++) dp[i][i] = 1;
-        // length 2
-        for (int i = 1; i < N; i++) if (arr[i] == arr[i + 1]) dp[i][i + 1] = 1;
+        arr = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
 
-        // length n;
-        for (int l = 3; l <= N; l++) // 길이가 l 일때
-            for (int s = 1; s <= N - l + 1; s++) // 시작 인덱스 s, 마지막 인덱스 e = s + l - 1;
-                if (arr[s] == arr[s + l - 1] && dp[s + 1][s + l - 2] == 1) // s와 e가 같고, s+1 부터 e-1 까지 팰린드롬인 경우
-                    dp[s][s + l - 1] = 1;
+        dp = new Integer[n+1][n+1];
 
-        M = Integer.parseInt(br.readLine());
-        for (int i = 1; i <= M; i++) {
-            line = br.readLine().split(" ");
-            int s = Integer.parseInt(line[0]);
-            int e = Integer.parseInt(line[1]);
-            sb.append(dp[s][e]).append('\n');
+        int t = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder(); // 출력 모으기
+        while (t-- > 0) {
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+
+            sb.append(dp(i, j)).append("\n");
         }
         System.out.print(sb);
+    }
+
+    private static int dp(int i, int j) {
+        if (i >= j) {
+            return dp[i][j] = 1;
+        }
+
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+
+        return dp[i][j] = arr[i] == arr[j] ? dp(i+1, j-1) : 0;
     }
 }
