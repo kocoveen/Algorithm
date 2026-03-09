1aa4e3087fdac00e19a2364468b0fc686fed0e78
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-03-09 18:41:58 +0900

    [Silver III] Title: 달팽이, Time: 600 ms, Memory: 56384 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -1,54 +1,82 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static int N, M;
+    static int[][] map;
+
     public static void main(String[] args) throws Exception {
-        int N = read();
-        int M = read();
+        Scanner sc = new Scanner(System.in);
 
-        int[][] map = new int[N][N];
-        int r = 0, c = 0, d = 0;
-        int curr = N * N;
+        N = sc.nextInt();
+        M = sc.nextInt();
 
-        int[] dr = {1, 0, -1, 0};
-        int[] dc = {0, 1, 0, -1};
+        map = new int[N][N];
 
-        int targetR = 0, targetC = 0;
+        int N2 = N * N;
 
-        while (curr > 0) {
-            map[r][c] = curr;
-            if (curr == M) {
-                targetR = r+1;
-                targetC = c+1;
+        int r = 0;
+        int c = 0;
+
+        int nr = 0, nc = 0;
+
+        int K = N+1;
+        while ((K = K - 2) > 0) {
+            // ↓
+            for (int n = 0; n < K; n++) {
+                if (N2 == M) {
+                    nr = r;
+                    nc = c;
+                }
+                map[r++][c] = N2--;
             }
 
-            if (curr == 1) break;
-
-            int nr = r + dr[d];
-            int nc = c + dc[d];
-
-            if (nr < 0 || nr >= N || nc < 0 || nc >= N || map[nr][nc] != 0) {
-                d = (d + 1) % 4;
-                nr = r + dr[d];
-                nc = c + dc[d];
+            // →
+            for (int n = 0; n < K; n++) {
+                if (N2 == M) {
+                    nr = r;
+                    nc = c;
+                }
+                map[r][c++] = N2--;
             }
-            r = nr;
-            c = nc;
-            curr--;
+
+            // ↑
+            for (int n = 0; n < K; n++) {
+                if (N2 == M) {
+                    nr = r;
+                    nc = c;
+                }
+                map[r--][c] = N2--;
+            }
+
+            // ←
+            for (int n = 0; n < K; n++) {
+                if (N2 == M) {
+                    nr = r;
+                    nc = c;
+                }
+                map[r][c--] = N2--;
+            }
+            
+            r++; c++;
         }
 
+        if (N2 == M) {
+            nr = r;
+            nc = c;
+        }
+
+        map[r][c] = N2;
+
         StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 sb.append(map[i][j]).append(" ");
             }
             sb.append("\n");
         }
-        sb.append(targetR).append(" ").append(targetC);
-        
-        System.out.println(sb.toString());
-    }
-
-    private static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
-        return n;
+        sb.append(nr+1).append(" ").append(nc+1);
+        System.out.print(sb);
     }
 }
