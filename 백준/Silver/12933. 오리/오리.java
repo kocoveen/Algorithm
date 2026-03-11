e4f37503f30b2915c1328384ac35a39b519932a8
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-03-11 12:56:55 +0900

    [Silver II] Title: 오리, Time: 176 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Silver/12933. 오리/오리.java
+++ 백준/Silver/12933. 오리/오리.java
@@ -1,28 +1,33 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
+    static String str;
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(solve(br.readLine()));
+        Scanner sc = new Scanner(System.in);
+        str = sc.next();
+
+        System.out.print(quack(str));
     }
 
-    static int solve(String s) {
-        String str = "quack";
-        int len = s.length();
+    private static int quack(String str) {
         int[] quack = new int[5];
-        for (int i = 0; i < len; i++) {
-            char c = s.charAt(i);
+        String s = "quack";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
             if (c == 'q') {
                 if (quack[4] > 0) quack[4]--;
                 quack[0]++;
             } else {
-                int j = str.indexOf(c);
+                int j = s.indexOf(c);
                 if (quack[j-1] == 0) return -1;
                 quack[j-1]--;
                 quack[j]++;
             }
         }
+
         if (quack[0] > 0 || quack[1] > 0 || quack[2] > 0 || quack[3] > 0) return -1;
         return quack[4];
     }
