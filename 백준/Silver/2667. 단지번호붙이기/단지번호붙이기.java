62990dfeb04d755d52a807e0241d0633c8e9759a
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-03-28 20:42:24 +0900

    [Silver I] Title: 단지번호붙이기, Time: 184 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,75 +1,74 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
+    
+    static int N, count;
+    static int[][] map;
+    static boolean[][] vis;
+    static List<Integer> list = new ArrayList<>();
 
-    static int N;
-
-    static int[][] board;
-    static boolean[][] visited;
-
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        board = new int[N][N];
-        visited = new boolean[N][N];
-        List<Integer> Area = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < N; j++)
-                board[i][j] = line.charAt(j) - '0';
+    static class Point {
+        int r, c;
+        Point(int r, int c) {
+            this.r = r;
+            this.c = c;
         }
+    }
 
-        int count = 0;
-        Queue<Pair> Q = new ArrayDeque<>();
+    static int[] dr = {1, 0, -1, 0};
+    static int[] dc = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        map = new int[N][N];
+        vis = new boolean[N][N];
+
         for (int i = 0; i < N; i++) {
+            String line = sc.next();
             for (int j = 0; j < N; j++) {
-                if (board[i][j] == 1 && !visited[i][j]) {
-                    int area = 0;
-                    Q.add(new Pair(i, j));
-                    visited[i][j] = true;
-                    count++; area++;
-                    while (!Q.isEmpty()) {
-                        Pair cur = Q.remove();
-                        for (int k = 0; k < 4; k++) {
-                            int nx = cur.x + dx[k];
-                            int ny = cur.y + dy[k];
-                            if (!isInBoard(nx, ny)) continue;
-                            if (board[nx][ny] == 0 || visited[nx][ny]) continue;
-                            Q.add(new Pair(nx, ny));
-                            visited[nx][ny] = true;
-                            area++;
-                        }
-                    }
-                    Area.add(area);
-                }
+                map[i][j] = line.charAt(j) - '0';
             }
         }
-        Collections.sort(Area);
-        System.out.println(count);
-        for (Integer i : Area)
-            System.out.println(i);
-    }
 
-    private static boolean isInBoard(int nx, int ny) {
-        return nx >= 0 && nx < N && ny >= 0 && ny < N;
-    }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (vis[i][j] || map[i][j] == 0) continue;
+                count = bfs(i, j);
+                list.add(count);
+            }
+        }
 
-    private static class Pair {
-        int x;
-        int y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
+        Collections.sort(list);
+        
+        System.out.println(list.size());
+        for (int elem : list) {
+            System.out.println(elem);
         }
     }
 
+    private static int bfs(int r, int c) {
+        Queue<Point> queue = new ArrayDeque<>();
+
+        queue.add(new Point(r, c));
+        vis[r][c] = true;
+
+        int count = 1;
+        while (!queue.isEmpty()) {
+            Point p = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = p.r + dr[i];
+                int nc = p.c + dc[i];
+                if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+                if (vis[nr][nc] || map[nr][nc] == 0) continue;
+                queue.add(new Point(nr, nc));
+                vis[nr][nc] = true;
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
