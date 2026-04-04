e58f7621ced2d2ce2047bd4a751847bda4c40b42
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-04-04 14:28:37 +0900

    [Gold IV] Title: 치즈, Time: 292 ms, Memory: 24460 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,71 +1,80 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
-    static int h, w, t, outerSize;
+
+    static int N, M;
     static int[][] map;
-    static boolean[][] vis;
-    static Queue<Info> q = new ArrayDeque<>();
-    static Queue<Info> outer = new ArrayDeque<>();
-
-    static class Info {
-        int r, c;
-        public Info(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-    }
-
+    
     static int[] dr = {1, 0, -1, 0};
     static int[] dc = {0, 1, 0, -1};
 
     public static void main(String[] args) throws Exception {
-        h = read(); w = read();
-        map = new int[h][w];
-        vis = new boolean[h][w];
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                map[i][j] = read();
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                map[i][j] = sc.nextInt();
             }
         }
 
-        q.add(new Info(0, 0));
-        vis[0][0] = true;
+        int t = 0;
+        int size = 0;
         while (true) {
-            q.addAll(outer);
-            outer.clear();
+            // 바깥 치즈 찾기
+            var list = getOutermostCheeses();
 
-            bfs();
+            if (list.isEmpty()) {
+                break;
+            }
 
-            if (outer.isEmpty()) break;
+            size = list.size();
 
-            outerSize = outer.size();
+            // 바깥 치즈 녹이기
+            melt(list);
             t++;
         }
+
         System.out.println(t);
-        System.out.println(outerSize);
+        System.out.println(size);
     }
 
-    static void bfs() {
-        while(!q.isEmpty()) {
-            Info cur = q.remove();
+    private static void melt(List<int[]> list) {
+        for (int[] c : list) {
+            map[c[0]][c[1]] = 0;
+        }
+    }
+
+    private static List<int[]> getOutermostCheeses() {
+        List<int[]> outermostList = new ArrayList<>();
+        
+        boolean[][] vis = new boolean[N][M];
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0, 0});
+        vis[0][0] = true;
+
+        while (!q.isEmpty()) {
+            int[] p = q.poll();
+
             for (int i = 0; i < 4; i++) {
-                int nr = cur.r + dr[i];
-                int nc = cur.c + dc[i];
-                if (nr < 0 || nc < 0 || nr >= h || nc >= w) continue;
+                int nr = p[0] + dr[i];
+                int nc = p[1] + dc[i];
+
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
                 if (vis[nr][nc]) continue;
 
-                if (map[nr][nc] != 1) q.add(new Info(nr, nc));
-                else outer.add(new Info(nr, nc));
+                if (map[nr][nc] == 1) {
+                    outermostList.add(new int[]{nr, nc});
+                } else {
+                    q.add(new int[]{nr, nc});
+                }
                 vis[nr][nc] = true;
             }
         }
 
-    }
-
-    static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
-        return n;
+        return outermostList;
     }
 }
