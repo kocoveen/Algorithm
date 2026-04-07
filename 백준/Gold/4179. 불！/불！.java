7980a5219bfa3546de55a833a8dc9c9d138b4e97
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-04-07 19:05:42 +0900

    [Gold III] Title: 불!, Time: 704 ms, Memory: 75424 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,82 +1,100 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int R, C;
+    static char[][] map;
 
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
-
-        st = new StringTokenizer(br.readLine());
-        int h = Integer.parseInt(st.nextToken());
-        int w = Integer.parseInt(st.nextToken());
-
-        char[][] building = new char[h][w];
-        int[][] fireTime = new int[h][w];
-        int[][] escapeTime = new int[h][w];
-
-        Queue<Pair> F = new ArrayDeque<>();
-        Queue<Pair> H = new ArrayDeque<>();
-
-        for (int i = 0; i < h; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < w; j++) {
-                char c = line.charAt(j);
-                building[i][j] = c;
-                fireTime[i][j] = escapeTime[i][j] = -1;
-
-                if (c == 'F') {
-                    F.add(new Pair(i, j));
-                    fireTime[i][j] = 0;
-                }
-
-                if (c == 'J') {
-                    H.add(new Pair(i, j));
-                    escapeTime[i][j] = 0;
-                }
-            }
-        }
-
-        while (!F.isEmpty()) {
-            Pair cur = F.remove();
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
-                if (fireTime[nx][ny] >= 0 || building[nx][ny] == '#') continue;
-                F.add(new Pair(nx, ny));
-                fireTime[nx][ny] = fireTime[cur.x][cur.y] + 1;
-            }
-        }
-
-        while (!H.isEmpty()) {
-            Pair cur = H.remove();
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                if (nx < 0 || nx >= h || ny < 0 || ny >= w) {
-                    System.out.println(escapeTime[cur.x][cur.y] + 1);
-                    System.exit(0);
-                }
-                if (escapeTime[nx][ny] >= 0 || building[nx][ny] == '#') continue;
-                if (fireTime[nx][ny] != -1 && fireTime[nx][ny] <= escapeTime[cur.x][cur.y] + 1) continue;
-                H.add(new Pair(nx, ny));
-                escapeTime[nx][ny] = escapeTime[cur.x][cur.y] + 1;
-            }
-        }
-        System.out.print("IMPOSSIBLE");
-    }
-
-    public static class Pair {
-        Integer x;
-        Integer y;
-
-        public Pair(Integer x, Integer y) {
-            this.x = x;
-            this.y = y;
+    static class Point {
+        int r, c;
+        Point(int r, int c) {
+            this.r = r;
+            this.c = c;
         }
     }
+
+    static List<Point> jhs = new ArrayList<>();
+    static List<Point> fires = new ArrayList<>();
+
+    static int[] dr = {1, 0, -1, 0};
+    static int[] dc = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        R = sc.nextInt();
+        C = sc.nextInt();
+        map = new char[R][C];
+        for (int r = 0; r < R; r++) {
+            String line = sc.next();
+            for (int c = 0; c < C; c++) {
+                map[r][c] = line.charAt(c);
+
+                if (map[r][c] == 'J') {
+                    jhs.add(new Point(r, c));
+                } else if (map[r][c] == 'F') {
+                    fires.add(new Point(r, c));
+                }
+            }
+        }
+
+        int[][] jihoon = bfs(jhs);
+        int[][] fire = bfs(fires);
+
+        // print(jihoon);
+        // print(fire);
+
+        int min = Integer.MAX_VALUE;
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                if (r != 0 && c != 0 && r != R-1 && c != C-1) continue;
+                if (jihoon[r][c] == 0) continue;
+
+                if (fire[r][c] == 0 || fire[r][c] > jihoon[r][c]) {
+                    min = Math.min(min, jihoon[r][c]);
+                }
+            }
+        }
+        System.out.println(min == Integer.MAX_VALUE ? "IMPOSSIBLE" : min);
+    }
+
+    private static int[][] bfs(List<Point> points) {
+        int[][] timeMap = new int[R][C];
+        Queue<Point> q = new ArrayDeque<>();
+        q.addAll(points);
+
+        for (Point p : points) {
+            timeMap[p.r][p.c] = 1;
+        }
+
+        while(!q.isEmpty()) {
+            Point p = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = p.r + dr[i];
+                int nc = p.c + dc[i];
+
+                if (nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
+                if (map[nr][nc] == '#') continue;
+                if (timeMap[nr][nc] > 0) continue;
+
+                int next = timeMap[p.r][p.c] + 1;
+                q.add(new Point(nr, nc));
+                timeMap[nr][nc] = next;
+            }
+        }
+
+        return timeMap;
+    }
+
+    // private static void print(int[][] map) {
+    //     for (int r = 0; r < R; r++) {
+    //         for (int c = 0; c < C; c++) {
+    //             IO.print("%2d ".formatted(map[r][c]));
+    //         }
+    //         IO.println();
+    //     }
+    //     IO.println();
+    // }
 }
