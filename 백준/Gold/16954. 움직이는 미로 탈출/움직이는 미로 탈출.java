aeb0cb8b959352e7a5a32935f5481a291d3db848
장웅기 <79296295+kocoveen@users.noreply.github.com>
2026-04-14 17:03:55 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 500 ms, Memory: 214440 KB -BaekjoonHub

--- 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Main {
 
     static int[][][] map;
-    static boolean[][][] vis;
 
     static class Info {
         int r, c, t;
@@ -21,7 +20,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         map = new int[8][8][8];
-        vis = new boolean[8][8][8];
 
         for (int i = 7; i >= 0; i--) {
             String line = sc.nextLine();
@@ -39,7 +37,7 @@ public class Main {
         while (!q.isEmpty()) {
             Info info = q.poll();
 
-            if (info.r == 7 && info.c == 7) {
+            if (info.t == 8) {
                 System.out.println(1);
                 System.exit(0);
             }
@@ -49,7 +47,6 @@ public class Main {
                 int nc = info.c + dc[d];
 
                 if (nr < 0 || nr > 7 || nc < 0 || nc > 7) continue;
-                if (info.t < 8 && vis[nr][nc][info.t]) continue;
                 if (info.t < 8 && map[nr][nc][info.t] == 1 
                     || (info.t < 7 && map[nr][nc][info.t + 1] == 1)) continue;
                 
